675cbbe28b45ce4b66449535dc918ac94ba62e9d
ngn <@>
2019-12-26 19:27:57 +0200

    b.c

--- b.c
+++ b.c
@@ -18,12 +18,13 @@ S I rhs(A x/*0*/)_(UH o=Ao(x);$(xts,P(gs(x)==4,h(bo);1)L i=fndl(fl,gs(x));P(i>=0
  P(u==cV('$')&&n>3,UC d[n];A*a=&xy;n--;F(n&~1,N(rhs(a[i]));h(i&1?bj:bz);d[i]=An(fb);h(0))N(rhs(n&1?a[n-1]:cu0));
                    A y=fb;F(n&~1,yc[d[i]]=(i&1?yn:d[i+1])-d[i])1)
  $(u==cU('*')&&n==2,A y=xy;P(ytX&&yn==2&&yx==cU('|'),N(rhs(yy));h(bu|29);1))$(utw&&n==2,A y=xy;P(!ytX&&!ytsS,yR;hc(app(u,&y,1));1))
- I p=0;F(n-1,A y=xa[n-1-i];$(y==cv_plc,p=1;hc(yR))E(N(rhs(y))))P(p,N(rhs(xx));h(bP);h(n-1);1)P(u==cv0&&n==2,h(br);1)P(u==cv_mkl,h(bl);h(n-1);1)
+ I p=0;F(n-1,A y=xa[n-1-i];y==cv_plc?(p=1),hc(yR):N(rhs(y)))P(p,N(rhs(xx));h(bP);h(n-1);1)P(u==cv0&&n==2,h(br);1)P(u==cv_mkl,h(bl);h(n-1);1)
  P(utu&&n==2,h(bu|Av(u));1)P(utv&&n==3,h(bv|Av(u));1)N(rhs(xx));h(ba);h(n-1);1)
 A1(cpl,UC k=Ak(x);f=x=mut(x);A y=fb;fb=fm=mR(aC(0));fc=a1(cu0);C o=0;F(16,lu[i]=-1)F(yn,P(!rhs(yai),xr;yr;0)h(i==yn-1?br:bp))yr;
  y=fb;F(16,I j=lu[i];$(ycj==bg,asrt(Ar(y)==1);ycj=bd))AK(k,AT(to,x)))
+
 S A sp0[0x10000],*sp=sp0+ZZ(sp0),*loc;S C*ip;
-S A1(vm_/*x0*/,W(1,UC b=*ip++;$(b>=bc,*--sp=mR(A(xa[4])[b&(mxc-1)]))
+S A vm_(A f/*0*/)_(W(1,UC b=*ip++;$(b>=bc,*--sp=mR(A(fc)[b&(mxc-1)]))
  E$(b>=bm,$(b==bm,A*p=loc-*ip++;A y=*sp++;*sp=N(ap2(mR(*p=Ny(dm1(A_(*p,yR,cv(*ip++),*sp),4))),y)))
          E$(b==bM,A x=*sp++;    A y=*sp++;*sp=N(ap2(      Ny(dmd(A_(x ,yR,cv(*ip++),*sp),4)) ,y)))
          E$(b==bl,UC n=*ip++;A x=sqz(atnv(tX,n,sp));sp+=n-1;*sp=x)
@@ -32,6 +33,6 @@ S A1(vm_/*x0*/,W(1,UC b=*ip++;$(b>=bc,*--sp=mR(A(xa[4])[b&(mxc-1)]))
          E$(b==bz,ip+=1+*ip*!tru(*sp++))E$(b==bj,ip+=*ip)E$(b==bo,*--sp=mR(loc[1]))E$(b==bp,mr(*sp++))E$(b==br,R*sp)E(UR))
  E$(b>=bs,A*p=loc-(b&(mxl-1));$(b>=bd,*--sp=*p;*p=cu0)E$(b>=bg,*--sp=mR(*p))E(mr(*p);*p=mR(*sp)))
  E$(b>=bv,sp++;N(*sp=((A2*)cf[b])(sp[-1],*sp)))E(N(*sp=((A1*)cf[b])(*sp))))UR;0)
-AX(run,asrt(xto);asrt(n==Ak(x));xR;I m=An(xz);P(sp<sp0+m+3,errn("stk",n,a))*--sp=(A)(V*)ip;*--sp=(A)(V*)loc;*--sp=x;ip=C(xy);loc=sp-1;
- F(n,*--sp=a[i])F(m-n,*--sp=cu0)A u=vm_(x);$(!u,eso(mR(xx),C(xa[3])[ip-1-C(xy)]);W(sp<loc-m,$(*sp,mr(*sp))sp++))
+AX(run,asrt(xto);asrt(n==Ak(x));A f=xR;I m=An(fl);P(sp<sp0+m+3,errn("stk",n,a))*--sp=(A)(V*)ip;*--sp=(A)(V*)loc;*--sp=f;ip=C(fb);loc=sp-1;
+ F(n,*--sp=a[i])F(m-n,*--sp=cu0)A u=vm_(f);$(!u,eso(mR(fs),C(fm)[ip-1-C(fb)]);W(sp<loc-m,$(*sp,mr(*sp))sp++))
  asrt(sp==loc-m);A*p=loc+1;W(p>sp,mr(*p--))sp=loc+4;ip=(C*)sp[-1];loc=(V*)sp[-2];u)
