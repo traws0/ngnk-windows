80ef4c1d17b2d40b7eafb09da37c6aa317921881
ngn <@>
2019-12-27 10:52:15 +0200

    j.c p.c - common code

--- j.c
+++ j.c
@@ -1,8 +1,8 @@
 #include"k.h" // ngn/k, (c) 2019 ngn, GNU AGPLv3 - http://bitbucket.org/ngn/k/raw/master/LICENSE
-S C*s;S A0 jv;S I dg(C c)_(C3('0',c,'9'))S C jw()_(W(Ci(" \n\t\r",*s)<4,++s)*s)
-S A0(jd,I m=*s=='-';s+=m;C c=*s++;P(!dg(c),0)D v=c-'0';W(dg(*s),v*=10;v+=*s++-'0')$(*s=='.',s++;D t=1;W(dg(*s),t/=10;v+=(*s++-'0')*t))
+S C*s;S A0 jv;S C jw()_(W(Ci(" \n\t\r",*s)<4,++s)*s)
+S A0(jd,I m=*s=='-';s+=m;C c=*s++;P(!dgt(c),0)D v=c-'0';W(dgt(*s),v*=10;v+=*s++-'0')$(*s=='.',s++;D t=1;W(dgt(*s),t/=10;v+=(*s++-'0')*t))
  $(*s=='e'||*s=='E',s++;s+=*s=='+';L e=pl(&s);D t=10;$(e<0,e=-e;t=.1)P(e>308,0)F(e,v*=t))ad(m?-v:v))
-S A1(ju,I v=0;F(4,C c=*s++;v=16*v+c-(dg(c)?'0':C3('a',c,'f')?'a'-10:C3('A',c,'F')?'A'-10:99999);P(v<0,pv(v);s--;0))
+S A1(ju,I v=0;F(4,C c=*s++;v=16*v+c-(dgt(c)?'0':C3('a',c,'f')?'a'-10:C3('A',c,'F')?'A'-10:99999);P(v<0,pv(v);s--;0))
  P(v<128,apc(x,v))P(v<2048,x=apc(x,192|v>>6);apc(x,128|63&v))x=apc(x,224|v>>12);x=apc(x,128|63&v>>6);apc(x,128|63&v))
 S A0(js,s++;A x=aC(0);W(1,C*t=s;UC c=*s++;W(c-'"'&&c-'\\'&&c>31,c=*s++)P(c<32,s--;xr;0)x=catc(x,t,s-1-t);P(c=='"',x)c=*s++;I i=Ci("\"\\/bfnrt",c);
                           $(i<8,x=apc(x,"\"\\/\b\f\n\r\t"[i]))E$(c=='u',x=ju(x))E(xr;s--;R 0)t=s)0)
@@ -10,5 +10,5 @@ S A0(ja,s++;A x=a0();P(jw()==']',s++;x)W(1,x=apd(x,Nx(jv()));P(*s==']',s++;x)Nx(
 S A0(jo,s++;A x=aa(aS(0),a0());C c=jw();P(c=='}',s++;x)
  W(1,Nx(c=='"');xx=apd(xx,gS(Nx(js())));Nx(jw()==':');s++;xy=apd(xy,Nx(jv()));P(*s=='}',s++;x)Nx(*s==',');s++;c=jw())0)
 S A jr(O C*t,L v)_(C c;W((c=*t++),P(c-*s,0)s++)al(v))
-S A0(jv,C c=jw();A x=c=='['?ja():c=='{'?jo():c=='"'?js():c=='-'||dg(c)?jd():c=='t'?jr("true",1):c=='f'?jr("false",0):c=='n'?jr("null",_0N):0;jw();x)
+S A0(jv,C c=jw();A x=c=='['?ja():c=='{'?jo():c=='"'?js():c=='-'||dgt(c)?jd():c=='t'?jr("true",1):c=='f'?jr("false",0):c=='n'?jr("null",_0N):0;jw();x)
 A1(json,P(!xtcC,et(x))x=str0(enla(x));s=xc;A u=jv();$(u&&*s,ur;u=0)P(!u,eso(x,s-xc);err("json");0)xr;u)
--- k.h
+++ k.h
@@ -84,7 +84,7 @@ TD void V;TD L A;TD A A0(),A1(A),A2(A,A),A3(A,A,A),AA(O A*,UC),AX(A,O A*,UC);
 #define min(x,y)extr(x,y,<)
 #define max(x,y)extr(x,y,>)
 SI L d2l(D v)_(*(L*)(V*)&v)SI L llabs(L x)_(x<0?-x:x)SI I C3(UC x,UC y,UC z)_(x<=y&&y<=z)
-SI D l2d(L v)_(*(D*)(V*)&v)SI C hex(I x)_(x+(x>9?'a'-10:'0'))
+SI D l2d(L v)_(*(D*)(V*)&v)SI C hex(I x)_(x+(x>9?'a'-10:'0'))SI I dgt(C c)_(C3('0',c,'9'))
 
 //   () "" ,0 ,l ,` ,d +!  ! 0W  0.  "a" 0 0j `  {} 1+ ++ +/ +: +  /
 enum{tX,tC,tI,tL,tS,tD,tA,ta,tlx,tdx,tc,ti,tl,ts,to,tp,tq,tr,tu,tv,tw,tn};
