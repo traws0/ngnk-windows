67941c2d8b0659c2d19f53f5a171b488323e65b8
ngn <@>
2020-01-10 02:55:22 +0200

    g/0.sh

--- g/0.sh
+++ g/0.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 cd "$(dirname "$0")"
 for f in ??*.k; do
- ( ulimit -v $((32*1024)) -t 1; ../k $f <<<'' || echo "exit code $?" ) | grep -v '^`ok$' | sed "1 i$f"
+ ( echo -n | ../k $f || echo "exit code $?" ) | grep -v '^`ok$' | sed "1i\\
+$f"
 done
 :
