4ef5b8225988f81b204cb88579022747ce6a441c
ngn <@>
2020-02-05 13:18:47 +0200

    s.c

--- s.c
+++ s.c
@@ -33,9 +33,9 @@ S C*sd(C*s,L d)_(UL x=d,m=x<<12>>12;I e=x<<1>>53;P(m&&e==2047,MC(s,"0n"))$(x>>63
  $(w0||u0,UC d=0;W((v/=10)>w/10,w0&=!dm(&w);u0&=!d;d=dm(&u);t++)$(w0,W(!dm(&w),u0&=!d;d=dm(&u);t++))$(u0&&d==5&&!(u&1),d=4)u+=d>=5||(u==w&&!(ev||w0)))
  E(UC d=0;W((v/=10)>(w/=10),d=dm(&u);t++)u+=u==w||d>=5)
  s++;I l=su(s,u)-s-1;s[-1]=*s;t+=l;
- P(0<=t&&t<16,I k=min(l,t);F(k,s[i]=s[i+1])ms(s+l,t-l,'0');s[t]='.';s+=l+t-k+1;$(t>=l,*s++='0')s)
+ P(0<=t&&t<16,I k=l<t?l:t;mc(s,s+1,k);ms(s+l,t-l,'0');s+=t;*s++='.';s+=l-k;$(t>=l,*s++='0')s)
  P(-4<t&&t<0,F(l+1,s[l-t-i]=s[l-i])s[-1]='0';*s++='.';ms(s,-t-1,'0')+l-t)
- $(l>0,*s++='.';s+=l)P(!t,s)*s++='e';$(t<0,*s++='-';t=-t)s4(s,t))
+ $(l>0,*s++='.';s+=l)*s++='e';$(t<0,t=-t;*s++='-')s4(s,t))
 A1(str0,asrt(xtC);x=room(x,1);xc[xn]=0;x)
 A1(str,P(xtl||xtd,UC l=xtl;L v=gl(x);A u=aC(24);AN((l?sl:sd)(uc,v)-uc,u))xtc?enl(x):xts?mR(symstr(gs(x))):xto?fir(AT(tX,x)):
  xtu||xtv?atnv(tC,1+(Av(x)>19)+xtu,(C[]){vc[Av(x)],':',':'}):xtw?atnv(tC,1+(Av(x)>2),(C[]){"'/\\'/\\"[Av(x)],':'}):xtt?kst(x):ea1(str,x))
