ce896d0d70239ea59fec752286bf38a9420e92aa
ngn <@>
2020-02-17 17:02:56 +0200

    rnd()

--- f.c
+++ f.c
@@ -1,10 +1,9 @@
 #include"k.h" // ngn/k, (c) 2019-2020 ngn, GNU AGPLv3 - http://bitbucket.org/ngn/k/raw/master/LICENSE
 //prng: xoshiro256+ (public domain) http://vigna.di.unimi.it/xorshift/ seeded with the central column of rule30, little-endian:
 // ","/"abcd",'"=0x",/:+"0123456789abcdef"@(16#16)\2/|+4 64#(n{(|(8#2)\30)@2/'3'0,x,0}\n=!2*n)@'n:256
-S A rndn(L n,L m)_(S UL a=0xd5a986ae75c9a33b,b=0x1016d8e3483a8f0f,c=0x81f9e6260eb8e5df,d=0xfa9b718d8d0769bf;
+S A rnd(L n,L m)_(S UL a=0xd5a986ae75c9a33b,b=0x1016d8e3483a8f0f,c=0x81f9e6260eb8e5df,d=0xfa9b718d8d0769bf;
  $(n==_0N,n=-m)A x=aL(absL(n));F(n,xli=a+d;O UL t=b<<17;c^=a;d^=b;b^=c;a^=d;c^=t;d=d<<45|d>>19)$(m&(m-1),F(xn,xli=(UL)xli%m))E$(m,F(xn,xli&=m-1))
- $(n<0,n=-n;P(n>m,el(x))F(n,L k=m-n+i;L v=((UL*)xl)[i]%=k+1;Fj(i,$(xlj==v,xli=k;B)))A y=rndn(n,0);F(n,L j=(UL)yli%(i+1);swp(xli,xlj))yr)x)
-S A2(rnd,L n=gl(x);P(ytl,rndn(n,gl(y)))P(ytc,C c=gc(y);P('a'-(32|c),ed())A z=rndn(n,26),u=aC(n);F(n,uci=c+zli)zr;u)P(ytT,idx(y,Ny(rndn(n,yn))))et(y))
+ $(n<0,n=-n;P(n>m,el(x))F(n,L k=m-n+i;L v=((UL*)xl)[i]%=k+1;Fj(i,$(xlj==v,xli=k;B)))A y=rnd(n,0);F(n,L j=(UL)yli%(i+1);swp(xli,xlj))yr)x)
 S L fnda(A x,A y/*0*/)_(asrt(xtX); F(xn,P(mtc_(xai,y),i))_0N)L fpa(A*p,A v)_(A x=*p;L n=xn;F(n,P(mtc_(xai,v),mr(v),i))*p=apd(x,v);n)
 S L fndc(A x,C v)_(asrt(tz(xt)==0);F(xn,P(xci==v,i))_0N)     L fpc(A*p,C v)_(A x=*p;L n=xn;F(n,P(xci==v,i))*p=apv(x,&v);n)
   L fndi(A x,I v)_(asrt(tz(xt)==2);F(xn,P(xii==v,i))_0N)     L fpi(A*p,I v)_(A x=*p;L n=xn;F(n,P(xii==v,i))*p=apv(x,&v);n)
@@ -12,7 +11,12 @@ S L fndc(A x,C v)_(asrt(tz(xt)==0);F(xn,P(xci==v,i))_0N)     L fpc(A*p,C v)_(A x
 S A1(uns,P(!xtC||xn-9||*xc-'d',ed(x))D v=*(D*)(xc+1);xr;ad(v))
 A2(fnd,
  P(xts,C*s=symptr(gs(x)),c=*s;P(s[1],ed(y))c=='j'?json(y):!c?uns(y):ed(y))
- P(xtl,rnd(x,y))
+ P(xtil,L n=gl(gL(x));
+        P(ytl,rnd(n,gl(y)))
+        P(yti,K("{`i$x?`l$y}",al(n),y))
+        P(ytc,C c=gc(y);P('a'-(32|c),ed())K("{`c$y+x?26}",al(n),ac(c)))
+        P(ytT,idx(y,Ny(rnd(n,yn))))
+        et(y))
  P(xtt,et(x,y))
  P(xta,A z=gkv(&x);idx(x,fnd(z,y)))
  P(xtA,en(x,y))
