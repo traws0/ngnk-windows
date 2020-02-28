1a873d417844e6a05e1d9b383a6eb4cae682dd1e
ngn <@>
2020-02-28 13:28:17 +0200

    cmd() dispatch table

--- c.c
+++ c.c
@@ -5,7 +5,7 @@ S A1(cmdw,asrt(xtC);L n=mu;P(!xn,xr;al(n))A y=Nx(val(xR));mr(out(y));n=mu-n;xr;n
 S A1(cmdt,asrt(xtC);C*s=xc;L n=*s==':'?++s,pl(&s):1;L t=now();x=N(cpl(N(prs(N(cut(al(s-xc),x))))));F(n,mr(Nx(app(x,0,0))))xr;al((now()-t+500)/1000))
 A1(cmd,P(!xtC,et(x))$(!xn||xn==1&&*xc=='\\',exit(0))C c=*xc;
  $(C3('a',c,'z')&&(xn==1||xc[1]==32||xc[1]==':'),I i=0;W(i<xn&&xci-32&&xci-':',i++)I j=i;W(j<xn&&xcj==32,j++)x=N(cut(al(j),x));
-  P(c=='l',ldf(x))P(c=='w',cmdw(x))P(c=='t',cmdt(x)))
+  S A1*t[26]={['l'-'a']=ldf,['t'-'a']=cmdt,['w'-'a']=cmdw};A1*f=t[c-'a'];P(f,f(x)))
  K("{0x0a\\`x(,\"/bin/sh\";x)}",x))
 A1(frk,P(!xtX||xn-2,ed(x))A y=gkv(&x);P(!xtX||!ytC,ed(x,y))x=mut(x);C*a[xn+1];F(xn,A z=xai;$(!ztC,ed(x,y))a[i]=C(xai=str0(z))) //`x((exe;a1;a2;..);in)
  a[xn]=0;xr;I p[4];pipe2(p,0);pipe2(p+2,0);P(!fork(),dup2(*p,0);dup2(p[3],1);F(4,close(p[i]))exit(execve(*a,a,0));0)close(*p);close(p[3]);
