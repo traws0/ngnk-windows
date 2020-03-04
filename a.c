92ea9ea929d67c3630ba5d1580664bcabf7a8b5f
ngn <@>
2020-03-04 11:14:54 +0200

    mod()

--- a.c
+++ a.c
@@ -56,7 +56,7 @@ S A pen2(A x,A y,I v)_(P(xta&&yta,A u=gkv(&y);am1(A_(x,y,av(v),u),4))P(xtaAX||yt
  Y(!k,t+=ti-tI;P(t==ti,I r;f(a,b,&r,1);xr;yr;ai(r)))P(xt==t&&Ar(x)==1,mr2(y,f(a,b,xc,xn);x))P(yt==t&&Ar(y)==1,mr2(x,f(a,b,yc,yn);y))
  A u=atn(t,k-1?xn:yn);mr2(x,mr2(y,f(a,b,uc,un);u)))
 A2(eql,xtsS&&ytsS?eql(AT(xt+ti-ts,x),AT(yt+ti-ts,y)):pen2(x,y,vi('=')))
-A2(mod,P(xtil,gl_(x)-_0Nl&&gi(x)-_0Ni?pen2(x,y,vi('!')):out(dex(x,y)))
+A2(mod,P(xtil,(xtl&&gl_(x)==_0Nl)||(xti&&gi(x)==_0Ni)?out(dex(x,y)):pen2(x,y,vi('!')))
  Y(xtt||xta,Y(xta,x=enl(x))x=rsh(al(len_(y)),x))Y(ytt||yta,Y(yta,y=enl(y))y=rsh(al(len_(x)),y))len_(x)-len_(y)?el(x,y):aa(x,y))
 #define h(c,s) A2(s,pen2(x,y,vi(c)))
 h('+',add)h('-',sub)h('*',mul)h('%',dvd)h('&',mnm)h('|',mxm)h('<',ltn)h('>',gtn)
