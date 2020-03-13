a38867c741127d94b18d0e0958cc2b3af1067ec4
ngn <@>
2020-03-13 17:05:53 +0200

    f.c

--- f.c
+++ f.c
@@ -4,7 +4,7 @@
 S A rnd(L n,L m)_(S UL a=0xd5a986ae75c9a33b,b=0x1016d8e3483a8f0f,c=0x81f9e6260eb8e5df,d=0xfa9b718d8d0769bf;
  Y(n==_0Nl,n=-m)A x=aL(absL(n));F(n,xli=a+d;O UL t=b<<17;c^=a;d^=b;b^=c;a^=d;c^=t;d=d<<45|d>>19)Y(m&(m-1),F(xn,xli=(UL)xli%m))EY(m,F(xn,xli&=m-1))
  Y(n<0,n=-n;P(n>m,el(x))F(n,L k=m-n+i;L v=((UL*)xl)[i]%=k+1;Fj(i,Y(xlj==v,xli=k;B)))A y=rnd(n,0);F(n,L j=(UL)yli%(i+1);swp(xli,xlj))yr)x)
-S L fnda(A x,A y/*0*/)_(asrt(xtX); F(xn,P(mtc_(xai,y),i))_0Nl)L fpa(A*p,A v)_(A x=*p;L n=xn;F(n,P(mtc_(xai,v),mr(v),i))*p=apd(x,v);n)
+S L fnda(A x,A y/*0*/)_(asrt(xtX); F(xn,P(mtc_(xai,y),i))_0Nl)L fpa(A*p,A y)_(A x=*p;L n=xn;F(n,P(mtc_(xai,y),yr;i))*p=apd(x,y);n)
 S L fndc(A x,C v)_(asrt(tz(xt)==0);F(xn,P(xci==v,i))_0Nl)     L fpc(A*p,C v)_(A x=*p;L n=xn;F(n,P(xci==v,i))*p=apv(x,&v);n)
   L fndi(A x,I v)_(asrt(tz(xt)==2);F(xn,P(xii==v,i))_0Nl)     L fpi(A*p,I v)_(A x=*p;L n=xn;F(n,P(xii==v,i))*p=apv(x,&v);n)
   L fndl(A x,L v)_(asrt(tz(xt)==3);F(xn,P(xli==v,i))_0Nl)     L fpl(A*p,L v)_(A x=*p;L n=xn;F(n,P(xli==v,i))*p=apv(x,&v);n)
