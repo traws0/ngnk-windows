f8adafa8d2d57f6a3187188adbc125729eb2fff3
ngn <@>
2020-03-15 20:00:20 +0200

    ser() funcs

--- x.c
+++ x.c
@@ -1,10 +1,11 @@
 #include"k.h" // ngn/k, (c) 2019-2020 ngn, GNU AGPLv3 - https://bitbucket.org/ngn/k/raw/master/LICENSE
 
-S A f8(V*v,A y)_(catc(y,v,8))
+S A f8(L v,A y)_(catc(y,(V*)&v,8))
 S A2(f,y=apc(y,xt);
  P(xts,en(x,y))
- P(pkd(x),f8(&x,y))
- P(xtX,y=Nx(f8(xc-8,y));F(xn,y=Nx(f(get(x,i),y)))y)
+ P(pkd(x),f8(x,y))
+ P(xtX,y=Nx(f8(xn,y));F(xn,y=Nx(f(mR(xai),y)))y)
+ P(xto||xtp||xtq||xtr,y=Nx(apc(y,Av(x)<<3|Ak(x)));f(AT(tX,x),y))
  P(xtC||xtI||xtlL||xtdD,I l=8*xtT;mr2(x,catc(y,xc-l,l+xn*tZ(xt))))
  en(x,y))
 A1(ser,f(x,aC(0)))
@@ -15,6 +16,7 @@ S A g()_(UC t=*s++;
  P(t==ts,en())
  P(pkdt(t),g8())
  P(t==tX,A x=atn(t,0);F(g8(),x=apd(x,Nx(g())))x)
+ P(t==to||t==tp||t==tq||t==tr,UC vk=*s++;A x=N(g());AT(t,AK(vk&7,AV(vk>>3,x))))
  P(t==tl||t==td||t==tC||t==tI||t==tL||t==tD,L n=t<ta?g8():1;C*v=s;s+=n*tZ(t);atnv(t,n,v))
  en())
 A1(des,P(!xtC,ed(x))s=xc;dex(x,g()))
