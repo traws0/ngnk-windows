e9c71bb2332699aee6d1c56dd1f747d96d67d383
ngn <@>
2020-03-15 17:48:53 +0200

    ser()

--- x.c
+++ x.c
@@ -1,3 +1,4 @@
 #include"k.h" // ngn/k, (c) 2019-2020 ngn, GNU AGPLv3 - https://bitbucket.org/ngn/k/raw/master/LICENSE
-A1(ser,en(x))
+S A2(f,en(x,y))
+A1(ser,f(x,aC(0)))
 A1(des,P(!xtC||xn-9||*xc-'d',ed(x))D v=*(D*)(xc+1);xr;ad(v))
