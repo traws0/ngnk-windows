e8da725a4b7fd2181ae3cf00012c4403ff5e7608
ngn <@>
2020-03-15 19:23:28 +0200

    ser() tX

--- x.c
+++ x.c
@@ -3,14 +3,17 @@
 S A2(f,y=apc(y,xt);
  P(xts,en(x,y))
  P(pkd(x),catc(y,(V*)&x,8))
+ P(xtX,catc(y,xc-8,8);F(xn,y=Nx(f(get(x,i),y)))y)
  P(xtC||xtI||xtlL||xtdD,I l=8*xtT;mr2(x,catc(y,xc-l,l+xn*tZ(xt))))
  en(x,y))
 A1(ser,f(x,aC(0)))
 
 S C*s;
+S L g8()_(L r=*(L*)s;s+=8;r)
 S A g()_(UC t=*s++;
  P(t==ts,en())
- P(pkdt(t),*(A*)(V*)s)
- P(t==tl||t==td||t==tC||t==tI||t==tL||t==tD,I l=t<ta;L n=l?*(L*)(V*)s:1;C*v=s+=8*l;s+=n*tZ(t);atnv(t,n,v))
+ P(pkdt(t),g8())
+ P(t==tX,A x=atn(t,0);F(g8(),x=apd(x,Nx(g())))x)
+ P(t==tl||t==td||t==tC||t==tI||t==tL||t==tD,L n=t<ta?g8():1;C*v=s;s+=n*tZ(t);atnv(t,n,v))
  en())
 A1(des,P(!xtC,ed(x))s=xc;dex(x,g()))
