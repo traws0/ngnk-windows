3c3d3ad01e08e48a7b9e6281afd08ca3d86de088
ngn <@>
2020-03-16 21:05:36 +0200

    x.c

--- x.c
+++ x.c
@@ -2,21 +2,19 @@
 
 S A f8(L v,A y)_(catc(y,(V*)&v,8))
 S A2(f,y=apc(y,xt);
- P(xts,en(x,y))
+ P(xtsS,en(x,y))
  P(pkd(x),f8(x,y))
  P(xtX,y=Nx(f8(xn,y));F(xn,y=Nx(f(mR(xai),y)));xr;y)
- P(xto||xtp||xtq||xtr,f(AT(tX,x),Nx(apc(y,Av(x)<<3|Ak(x)))))
- P(xtC||xtI||xtlL||xtdD,I l=8*xtT;mr2(x,catc(y,xc-l,l+xn*tZ(xt))))
- en(x,y))
+ P(fun(x),f(AT(tX,x),Nx(apc(y,Av(x)<<3|Ak(x)))))
+ I l=8*xtT;mr2(x,catc(y,xc-l,l+xn*tZ(xt))))
 A1(ser,f(x,aC(0)))
 
 S C*s;
 S L g8()_(L r=*(L*)s;s+=8;r)
 S A g()_(UC t=*s++;
- P(t==ts,en())
+ P(t==ts||t==tS,en())
  P(pkdt(t),g8())
- P(t==tX,A x=atn(t,0);F(g8(),x=apd(x,Nx(g())))x)
- P(t==to||t==tp||t==tq||t==tr,UC vk=*s++;A x=N(g());AT(t,AK(vk&7,AV(vk>>3,x))))
- P(t==tl||t==td||t==tC||t==tI||t==tL||t==tD,L n=t<ta?g8():1;C*v=s;s+=n*tZ(t);atnv(t,n,v))
- en())
+ P(t==tX,L n=g8();A x=AN(0,atn(t,n));F(n,x=apd(x,Nx(g())))x)
+ P(funt(t),UC vk=*s++;A x=N(g());AT(t,AK(vk&7,AV(vk>>3,x))))
+ L n=t<ta?g8():1;C*v=s;s+=n*tZ(t);atnv(t,n,v))
 A1(des,P(!xtC,ed(x))s=xc;dex(x,g()))
