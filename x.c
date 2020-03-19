281dbed3393efc0888703f039b640dabc77f5965
ngn <@>
2020-03-19 18:21:33 +0200

    x.c

--- x.c
+++ x.c
@@ -1,19 +1,19 @@
 #include"k.h" // ngn/k, (c) 2019-2020 ngn, GNU AGPLv3 - https://bitbucket.org/ngn/k/raw/master/LICENSE
 
-S A f8(L v,A y)_(catc(y,(V*)&v,8))
-S A2(f,y=Nx(apc(y,xt));
- P(xts,f(mR(symstr(gi(x))),y))
- P(pkd(x),f8(x,y))
- P(xtX||xtS,y=Nx(f8(xn,y));F(xn,y=Nx(f(get(x,i),y)))xr;y)
- P(fun(x),f(AT(tX,x),Nx(apc(y,Av(x)<<3|Ak(x)))))
+S A ser8(L v,A y)_(catc(y,(V*)&v,8))
+S A2(serx,y=Nx(apc(y,xt));
+ P(xts,serx(mR(symstr(gi(x))),y))
+ P(pkd(x),ser8(x,y))
+ P(xtX||xtS,y=Nx(ser8(xn,y));F(xn,y=Nx(serx(get(x,i),y)))xr;y)
+ P(fun(x),serx(AT(tX,x),Nx(apc(y,Av(x)<<3|Ak(x)))))
  I l=8*xtT;mr2(x,catc(y,xc-l,l+xn*tZ(xt))))
-A1(ser,f(x,aC(0)))
+A1(ser,serx(x,aC(0)))
 
-S L g8(C**p)_(L r=*(L*)*p;*p+=8;r)
-S A g(C**p)_(UC t=*(*p)++;
- P(t==ts,as(sym(N(g(p)))))
- P(pkdt(t),g8(p))
- P(t==tX||t==tS,L n=g8(p);A x=AN(0,atn(t,n));F(n,x=apd(x,Nx(g(p))))x)
- P(funt(t),UC vk=*(*p)++;A x=N(g(p));AT(t,AK(vk&7,AV(vk>>3,x))))
- L n=t<ta?g8(p):1;C*v=*p;(*p)+=n*tZ(t);atnv(t,n,v))
-A1(des,P(!xtC,ed(x))C*s=xc;dex(x,g(&s)))
+S L des8(C**p)_(L r=*(L*)*p;*p+=8;r)
+S A desx(C**p)_(UC t=*(*p)++;
+ P(t==ts,as(sym(N(desx(p)))))
+ P(pkdt(t),des8(p))
+ P(t==tX||t==tS,L n=des8(p);A x=AN(0,atn(t,n));F(n,x=apd(x,Nx(desx(p))))x)
+ P(funt(t),UC vk=*(*p)++;A x=N(desx(p));AT(t,AK(vk&7,AV(vk>>3,x))))
+ L n=t<ta?des8(p):1;C*v=*p;(*p)+=n*tZ(t);atnv(t,n,v))
+A1(des,P(!xtC,ed(x))C*s=xc;dex(x,desx(&s)))
