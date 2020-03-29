8c36b3ff2d3ada58d0370e923041b263cbe47763
ngn <@>
2020-03-29 17:20:42 +0300

    asc()

--- o.c
+++ o.c
@@ -6,10 +6,10 @@ S I cmp_(A x,A y/*x0y0*/)_(P(x==y,0)I d=xt-yt;P(d,d)P(xti||xtc,(L)gi(x)-(L)gi(y)
 A2(mtc,I r=mtc_(x,y);xr;yr;ai(r))
 S L bef(A*a,L i,L j)_(I r=cmp_(a[i],a[j]);r?r<0:i<j)
 S V sft(A*a,L*l,L i,L j)_(W(1,L k=1+2*i;Y(k>j,B)Y(k+1<=j&&bef(a,l[k],l[k+1]),k++)Y(!bef(a,l[i],l[k]),B)SWP(l[i],l[k])i=k))
-S A1(ascil,P(xn>_0Wi,err("lmt",x))x=N(K("{x-&/x}",x));I w=tZ(xt);UI n=xn;A y=aC(n),z=aI(n),u=aI(n);UC*a=dat(y);UI c[257];tili(ui,n);
+S A1(asci,P(xn>_0Wi,err("lmt",x))x=N(K("{x-&/x}",x));I w=tZ(xt);UI n=xn;A y=aC(n),z=aI(n),u=aI(n);UC*a=dat(y);UI c[257];tili(ui,n);
  mr2(x,mr2(y,Fj(w,ms(c,0,Z c);F(n,a[i]=xc[w*uii+j])F(n,c[a[i]+1]++)Y(c[1+*a]-n,F(255,c[i+1]+=c[i])F(n,zi[c[a[i]]++]=uii)SWP(z,u)))zr;u)))
 A1(asc,P(xtX,A*a=xa,u=aL(xn);till(ul,un);L*l=ul,n=xn,i=n/2;W(i-->0,sft(a,l,i,n-1))i=n-1;W(i>0,SWP(l[i],*l)sft(a,l,0,--i))xr;u)
- P(xtIL,ascil(x))P(xta,A y=gkv(&x);idx(x,N(asc(y))))xtC||xtI?asc(N(gL(x))):xtS?asc(N(ea1(str,x))):xtD?asc(blw(x)):xts||xti?hop(x):et(x))
+ P(xta,A y=gkv(&x);idx(x,N(asc(y))))xtIL||xtC?asci(x):xtS?asc(N(ea1(str,x))):xtD?asc(blw(x)):xts||xti?hop(x):et(x))
 A1(dsc,P(xti,hcl(x))P(xta,A y=gkv(&x);idx(x,N(dsc(y))))sub(al(len_(x)-1),rev(asc(rev(x)))))
 A1(grp,P(xtt,xtil?K("{(2#x)#1i,`i$&x}",x):et(x))P(xta,A u=grp(gkv(&x));asrt(Ar(u)==1);uy=idx(x,uy);u)A y=atn(xt,0),u=a0();UC w=tz(xt);
  F(xn,L j=w==ZR?fpa(&y,mR(xai)):!w?fpc(&y,xci):w==2?fpi(&y,xii):w==3?fpl(&y,xli):0;Y(j==un,u=apd(u,aL(0)))uaj=apv(uaj,&i))xr;aa(y,u))
