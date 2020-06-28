93500f8270f286c7eae9230190cba3e57fa1db02
ngn <@>
2020-06-28 22:08:22 +0300

    .

--- c.c
+++ c.c
@@ -27,17 +27,16 @@ S V sha1u(UI*d,UL n,UI*a){
   *a+=x;a[1]+=y;a[2]+=z;a[3]+=u;a[4]+=v;d+=16)}
 A1(sha1,mdc(x,iv,5,sha1u,1))
 
-#define h(x0,x1,x2,x3,x4,x5,x6,x7) {UI t=x7+(rol(x4,26)^rol(x4,21)^rol(x4,7))+ch(x4,x5,x6)+k[r]+w[r],u=(rol(x0,30)^rol(x0,19)^rol(x0,10))+maj(x0,x1,x2);x3+=t;x7=t+u;r++;}
-S V sha256u(UI*d,UL n,UI*a){S O UI k[]={0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,0xd807aa98,0x12835b01,
+#define h(a,b,c,d,e,f,g,h) {UI t=h+(rol(e,26)^rol(e,21)^rol(e,7))+ch(e,f,g)+k[r]+w[r],u=(rol(a,30)^rol(a,19)^rol(a,10))+maj(a,b,c);d+=t;h=t+u;r++;}
+S V sha256u(UI*v,UL n,UI*s){S O UI k[]={0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,0xd807aa98,0x12835b01,
  0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
  0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,
  0x81c2c92e,0x92722c85,0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,
  0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2};
- F(n,UI w[64];be4n(w,d,16);F(48,UI p=w[i+1],q=w[i+14];w[i+16]=w[i]+(rol(p,25)^rol(p,14)^p>>3)+w[i+9]+(rol(q,15)^rol(q,13)^q>>10))
-  UI x0=*a,x1=a[1],x2=a[2],x3=a[3],x4=a[4],x5=a[5],x6=a[6],x7=a[7],r=0;
-  F(8,h(x0,x1,x2,x3,x4,x5,x6,x7)h(x7,x0,x1,x2,x3,x4,x5,x6)h(x6,x7,x0,x1,x2,x3,x4,x5)h(x5,x6,x7,x0,x1,x2,x3,x4)
-      h(x4,x5,x6,x7,x0,x1,x2,x3)h(x3,x4,x5,x6,x7,x0,x1,x2)h(x2,x3,x4,x5,x6,x7,x0,x1)h(x1,x2,x3,x4,x5,x6,x7,x0))
-  *a+=x0;a[1]+=x1;a[2]+=x2;a[3]+=x3;a[4]+=x4;a[5]+=x5;a[6]+=x6;a[7]+=x7;d+=16)}
+ F(n,UI w[64];be4n(w,v,16);F(48,UI p=w[i+1],q=w[i+14];w[i+16]=w[i]+(rol(p,25)^rol(p,14)^p>>3)+w[i+9]+(rol(q,15)^rol(q,13)^q>>10))
+  UI a=*s,b=s[1],c=s[2],d=s[3],e=s[4],f=s[5],g=s[6],h=s[7],r=0;
+  F(8,h(a,b,c,d,e,f,g,h)h(h,a,b,c,d,e,f,g)h(g,h,a,b,c,d,e,f)h(f,g,h,a,b,c,d,e)h(e,f,g,h,a,b,c,d)h(d,e,f,g,h,a,b,c)h(c,d,e,f,g,h,a,b)h(b,c,d,e,f,g,h,a))
+  *s+=a;s[1]+=b;s[2]+=c;s[3]+=d;s[4]+=e;s[5]+=f;s[6]+=g;s[7]+=h;v+=16)}
 A1(sha256,mdc(x,(UI[]){0x6a09e667,0xbb67ae85,0x3c6ef372,0xa54ff53a,0x510e527f,0x9b05688c,0x1f83d9ab,0x5be0cd19},8,sha256u,1))
 #undef h
 
