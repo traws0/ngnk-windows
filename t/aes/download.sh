46705958d0a63ff18bf1de5e2839c44c7ea29b20
ngn <@>
2020-08-07 23:31:39 +0300

    .

--- t/aes/download.sh
+++ t/aes/download.sh
@@ -3,5 +3,5 @@ set -e
 z=kat_aes.zip
 mkdir kat
 curl "https://csrc.nist.gov/csrc/media/projects/cryptographic-algorithm-validation-program/documents/aes/$z" >$z
-sha256sum -c <<<'a203b16c9246b2ebae31dee5de21a606be80cf78ceabaca37150236fa098eb60  kat_aes.zip'
+sha256sum -c <<<"a203b16c9246b2ebae31dee5de21a606be80cf78ceabaca37150236fa098eb60 $z"
 unzip $z -d kat
