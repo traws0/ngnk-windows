cb64af6526c317c5c54a6779cf5f079d716903d5
ngn <@>
2020-08-26 01:00:25 +0300

    sha256

--- c.c
+++ c.c
@@ -19,18 +19,20 @@ S A mdc(A x,O V*iv,I nv,V(*f)(U*,UL,U*),I b)_(Et(!xtC)A u=aCn(iv,4*nv);UL n=xn,k
  C c[128];m2(x,f(xi,k,ui);mc(c,xc+n-r,r));c[r]=128;ms(c+r+1,0,m-n-9);*(UL*)(V*)(c+m-n+r-8)=b?bL(8*n):8*n;f((V*)c,m/64-k,ui);Y(b,bn(ui,ui,nv))u)
 S O U iv[]={0x67452301,0xefcdab89,0x98badcfe,0x10325476,0xc3d2e1f0};A1(md5,Y(!*md5k,md5i())mdc(x,iv,4,md5u,0))A1(sha1,mdc(x,iv,5,sha1u,1))
 
-#define h(a,b,c,d,e,f,g,h) {d+=h+=(rI(e,26)^rI(e,21)^rI(e,7))+chx(e,f,g)+k[r]+w[r];h+=(rI(a,30)^rI(a,19)^rI(a,10))+maj(a,b,c);r++;}
+#define F8(a...) {{I i=0;a;}{I i=1;a;}{I i=2;a;}{I i=3;a;}{I i=4;a;}{I i=5;a;}{I i=6;a;}{I i=7;a;}}
+#define hh(a,b,c,d,e,f,g,h) {d+=h+=(rI(e,26)^rI(e,21)^rI(e,7))+chx(e,f,g)+k[r]+w[r];h+=(rI(a,30)^rI(a,19)^rI(a,10))+maj(a,b,c);r++;}
+#define h(j) hh(t[-j&7],t[1-j&7],t[2-j&7],t[3-j&7],t[4-j&7],t[5-j&7],t[6-j&7],t[7-j&7])
 S V sha256u(U*v,UL n,U*s){S O U k[]={0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,0xd807aa98,0x12835b01,
  0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
  0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,
  0x81c2c92e,0x92722c85,0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,
  0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2};
  F(n,U w[64];bn(w,v,16);F(48,U p=w[i+1],q=w[i+14];w[i+16]=w[i]+(rI(p,25)^rI(p,14)^p>>3)+w[i+9]+(rI(q,15)^rI(q,13)^q>>10))
-  U a=*s,b=s[1],c=s[2],d=s[3],e=s[4],f=s[5],g=s[6],h=s[7],r=0;
-  F(8,h(a,b,c,d,e,f,g,h)h(h,a,b,c,d,e,f,g)h(g,h,a,b,c,d,e,f)h(f,g,h,a,b,c,d,e)h(e,f,g,h,a,b,c,d)h(d,e,f,g,h,a,b,c)h(c,d,e,f,g,h,a,b)h(b,c,d,e,f,g,h,a))
-  *s+=a;s[1]+=b;s[2]+=c;s[3]+=d;s[4]+=e;s[5]+=f;s[6]+=g;s[7]+=h;v+=16)}
+     U r=0,t[8];F8(t[i]=s[i])F(8,h(0)h(1)h(2)h(3)h(4)h(5)h(6)h(7))F8(s[i]+=t[i])v+=16)}
 A1(sha256,mdc(x,(U[]){0x6a09e667,0xbb67ae85,0x3c6ef372,0xa54ff53a,0x510e527f,0x9b05688c,0x1f83d9ab,0x5be0cd19},8,sha256u,1))
 #undef h
+#undef hh
+#undef F8
 
 #define F5(a...) {{I i=0;a;}{I i=1;a;}{I i=2;a;}{I i=3;a;}{I i=4;a;}}
 S V ks(UL*a){S O UL z=1<<15,y=1ll<<31,x=1ll<<63,t[]={1,z+130,x+z+138,x+y+z,z+139,y+1,x+y+z+129,x+z+9,138,136,y+z+9,y+10,y+z+139, //keccak sponge
