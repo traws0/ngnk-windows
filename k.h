49b23403f0f022f3b22d4a0c436e13b11d156fd4
ngn <@>
2020-09-27 20:45:30 +0300

    k.h

--- k.h
+++ k.h
@@ -1,1 +1,4 @@
 // ngn/k, (c) 2019-2020 ngn, GNU AGPLv3 - https://bitbucket.org/ngn/k/raw/master/LICENSE
+typedef unsigned long long A;
+A app(A,const A*,int);
+#define K(s,a...) ({static A f;if(!f)f=K0(s,sizeof s);sizeof(A[]){a}?app(f,(A[]){a},sizeof((A[]){a})/sizeof(A)):f;})
