7f0bca73d4c181bece1bf6276c110fa85d948124
ngn <@>
2020-09-28 13:37:08 +0300

    gi()

--- o.c
+++ o.c
@@ -1,7 +1,7 @@
 #include"a.h" // ngn/k, (c) 2019-2020 ngn, GNU AGPLv3 - https://bitbucket.org/ngn/k/raw/master/LICENSE
 I mtc_(A x,A y/*x0y0*/)_(P(x==y,1)P(xt-yt||pkd(x)||xn-yn||(xtr&&xv-yv),0)P(!ref(x),F(xn*ZT[xt],P(xci-yci,0))1)F(xn,P(!mtc_(xai,yai),0))1)
 S I cmpL(L x,L y)_(x<y?-1:x>y)
-S I cmp_(A x,A y/*x0y0*/)_(P(x==y,0)I d=xt-yt;P(d,d)P(xti||xtc,(L)gi(x)-(L)gi(y))P(xtl,cmpL(gl_(x),gl_(y)))P(xtd,D u=gd_(x),v=gd_(y);u<v?-1:u>v?1:0)
+S I cmp_(A x,A y/*x0y0*/)_(P(x==y,0)I d=xt-yt;P(d,d)P(xti,gi(x)-gi(y))P(xtc,gc(x)-gc(y))P(xtl,cmpL(gl_(x),gl_(y)))P(xtd,D u=gd_(x),v=gd_(y);u<v?-1:u>v?1:0)
  P(xts,strcmp(syp(gs(x)),syp(gs(y))))Y(!pkd(x),I r=cmpL(xn,yn);P(xtT,F(min(xn,yn),A z=get(x,i),u=get(y,i);I d=cmp_(z,u);zr;ur;P(d,d))r))cmpL(x,y))
 A2(mtc,I r=mtc_(x,y);xr;yr;ai(r))
 S I bef(A*a,I i,I j)_(I r=cmp_(a[i],a[j]);r?r<0:i<j)
