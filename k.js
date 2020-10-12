9b052bf2b0e0c656986e3080c54f721bdf79878c
ngn <@>
2020-10-12 06:43:42 +0300

    k.js syscalls

--- k.js
+++ k.js
@@ -1,29 +1,34 @@
-let K,D,M,H,S
-const N='\n',te=new TextEncoder('utf-8'),td=new TextDecoder('utf-8')
-,uTA=_=>{let n=ta.value.length;ta.setSelectionRange(n,n)}
-,uMD=_=>{let b=K.memory.buffer;M=new Uint8Array(b);D=new DataView(b)}
-,s4=(x,y)=>D.setUint32(x,y,1)
-,S4=(x,y)=>{for(let i=0;i<y.length;i++){s4(x,y);x+=4}}
-,nyi=x=>(alert('nyi: '+x),-1)
-,env={
-  mmap:(_,n)=>{H+=n;if(H>M.length){try{K.memory.grow((H-M.length-1>>>16)+1);uMD()}catch(e){alert('OOM')}}return H-n}
- ,munmap:_=>0
- ,read:(_,a,n)=>{let s=S||prompt('stdin:')+N;S='';return te.encodeInto(s,M.subarray(a,a+n)).written}
- ,write:(_,a,n)=>{ta.value+=td.decode(M.subarray(a,a+n));uTA();return n}
- ,gettimeofday:x=>{let t=Date.now();S4(x,[t/1000|0,t%1000*1000]);return 0}
- ,open   :_=>nyi('open'   )
- ,close  :_=>nyi('close'  )
- ,lseek  :_=>nyi('lseek'  )
- ,dup2   :_=>nyi('dup2'   )
- ,pipe   :_=>nyi('pipe'   )
- ,execve :_=>nyi('execve' )
- ,fork   :_=>nyi('fork'   )
- ,exit   :_=>nyi('exit'   )
- ,socket :_=>nyi('socket' )
- ,connect:_=>nyi('connect')}
+const ba='ngn/k, (c) 2019-2020 ngn, GNU AGPLv3 - https://bitbucket.org/ngn/k/raw/master/LICENSE\n',pr=' ',
+te=new TextEncoder('utf-8'),td=new TextDecoder('utf-8'),
+ap=s=>{ta.value+=s;let n=ta.value.length;ta.setSelectionRange(n,n)},
+upd=_=>D=new DataView(K.memory.buffer),M=(p,n)=>new Uint8Array(K.memory.buffer).subarray(p,p+n),
+g1=p=>D.getUint8(p),gb=p=>{let q=p;while(g1(q))q++;return M(p,q-p)},gs=p=>td.decode(gb(p)),
+s4=(p,x)=>D.setUint32(p,x,1),S4=(p,a)=>{for(let i=0;i<a.length;i++)s4(p+4*i,a[i])},
+fs={},fd=Array(7),env={},E=x=>~console.error(x),N=x=>(alert(x='nyi:'+x),E(x)),log=x=>console.log(x),
+popn=a=>{while(a.length&&a[a.length-1]==null)a.pop();return a},
+S=(s,f)=>env[s]=(...a)=>{log(s+'('+popn(a)+')');let r=f(...a);log(s+'(..)='+r);return r} //strace
+//S=(s,f)=>env[s]=f //no strace
+let K,D,H,I='' //K:wasmapp,D:dataview(memory),H:heappointer,I:pendinginput
+
+S('mmap',(p,n,_,_1,f,o)=>{
+ if(!p){H+=n;let m=K.memory,l=m.buffer.byteLength;if(H>l){try{m.grow((H-l-1>>>16)+1)}catch(e){return E('OOM')}}upd();p=H-n}
+ if(f>=0){f=fd[f];if(!f)return E('bad fd');let a=fs[f.p],b=M(p,n);for(let i=o;i<n;i++)b[i]=a[i]}return p})
+S('munmap',_=>0)
+S('read',(f,a,n)=>{if(f<3){let s=I||prompt('stdin:')+'\n';I='';return te.encodeInto(s,M(a,n)).written}return N('read(f)')})
+S('write',(f,a,n)=>f<3?(ap(td.decode(M(a,n))),n):N('write(f)'))
+S('gettimeofday',x=>S4(x,[(x=Date.now())/1000|0,x%1000*1000])&0)
+S('open',(p,u,_)=>{p=gs(p);let f=3;while(fd[f])f++;if(f>fd.length)return E('max fd')
+ if(!fs[p]&&!(u&64/*O_CREAT*/))return E('file not found')
+ if(!fs[p]||(u&512/*O_TRUNC*/))fs[p]=new Uint8Array(0)
+ fd[f]={p,o:0};return f})
+S('close',f=>fd[f]?fd[f]=0:E('bad fd'))
+S('lseek',(f,o,w)=>(f=fd[f])?f.o=o+(!w?0:w===1?f.o:fs[f.p].length):E('bad fd'))
+S('exit',x=>{alert(x='exit('+x+')');ta.disabled=1;throw Error(x)})
+'dup2,pipe,execve,fork,socket,connect'.split(',').map(x=>S(x,_=>N(x)))
+
 WebAssembly.instantiateStreaming(fetch('k.wasm'),{env})
- .then(x=>{K=x.instance.exports;uMD();H=K.__heap_base;K.init()})
-onload=_=>{ta.onkeydown=x=>{if(x.which===13){
- let s=ta.value+N,p=ta.selectionStart,q=ta.selectionEnd
- if(p===q){p=s.slice(0,p).lastIndexOf(N)+1;q+=s.slice(q).indexOf(N)+1}
- S=s.slice(p,q);ta.value+=q===s.length?N:S;uTA();K.rep();return!1}}}
+           .then(x=>{K=x.instance.exports;upd();H=K.__heap_base;K.init();ta.disabled=0;ap(ba+pr);ta.focus()})
+onload=_=>ta.onkeydown=x=>{if(x.which===13){
+ let s=ta.value+'\n',p=ta.selectionStart,q=ta.selectionEnd
+ if(p===q){p=s.slice(0,p).lastIndexOf('\n')+1;q+=s.slice(q).indexOf('\n')+1}
+ I=s.slice(p,q);ap(q===s.length?'\n':I);K.rep();ap(pr);return!1}}
