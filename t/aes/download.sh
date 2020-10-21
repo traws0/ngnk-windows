98b9149247de04ac5be7a057505ffaf8d6e0f190
ngn <@>
2020-10-21 12:42:56 +0300

    .

--- t/aes/download.sh
+++ t/aes/download.sh
@@ -3,7 +3,7 @@ set -e
 z=kat_aes.zip
 mkdir kat
 curl "https://csrc.nist.gov/csrc/media/projects/cryptographic-algorithm-validation-program/documents/aes/$z" >$z
-if [ "$(echo "1@\`hex@\`sha256@1:\"$z\";\n\\" | ../../k)" != a203b16c9246b2ebae31dee5de21a606be80cf78ceabaca37150236fa098eb60 ]; then
+if [ "$(echo "\`sha256@1:\"$z\"\n\\" | ../../k)" != 0xa203b16c9246b2ebae31dee5de21a606be80cf78ceabaca37150236fa098eb60 ]; then
  echo "ERROR: $z checksum did not match"; exit 1
 fi
 unzip $z -d kat
