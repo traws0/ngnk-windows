3fd7ef6f85c1e2631bb36adbc5b94b5066f72f6d
ngn <@>
2020-10-27 00:52:34 +0200

    web.c

--- web/web.c
+++ web/web.c
@@ -26,19 +26,17 @@
 #define XS(x) MS(x)
 typedef void V;typedef char C;typedef int I;typedef unsigned short UH;typedef unsigned UI;typedef unsigned long long UL;
 
-S C*sk(C*s,C c)_(W(*s==c,s++)s) //sk:skip
-S C*su(C*s,UL x)_(Y(x>9,s=su(s,x/10))*s++='0'+x%10;s)
+S C*sk(C*s,C c)_(W(*s==c,s++)s)S C*su(C*s,UL x)_(Y(x>9,s=su(s,x/10))*s++='0'+x%10;s)
 S I die(O C*s)_(I e=errno;wrZ(1,"ERR: ");wr(1,s,mn(s));wrZ(1," -> ");s=strerror(e);wr(1,s,mn(s));wrZ(1,"\n");exit(e);0)
 S O C*m[][2]={{"html","text/html"},{"js","application/javascript"},{"png","image/png"},{"wasm","application/wasm"}};
 S O C*mime(O C*x)_(F(ZZ(m),P(!strcmp(x,*m[i]),m[i][1]))(V*)0)
-S V r404(I f){O C*s="HTTP/1.1 404 Not Found\nContent-Length:4\nConnection:close\nContent-Type:text/html\n\n404\n";wrZ(f,s);}
+S V r404(I f){wrZ(f,"HTTP/1.1 404 Not Found\nContent-Length:4\nConnection:close\nContent-Type:text/html\n\n404\n");}
+S V ap(C**p,O C*s){I n=mn(s);mc(*p,s,n);*p+=n;}
 S O C*web(I f)_(C b[1024];I r=read(f,b,Z b-1);P(r<=0,"read failed")b[r]=0;*sc0(b,10)=0;*sc0(b,13)=0;
  wr(1,b,mn(b));wrZ(1," - ");P(strncmp(b,"GET ",4),"not GET")C*s=b+4;s=sk(s,32);s+=*s=='/';*sc0(s,32)=0;
  Y(!*s,s="index.html")O C*d=sc0(s,'.'),*m=*d?mime(d+1):0;P(!m,"bad ext")
- I g=open(s,O_RDONLY);P(g<0,"not found")I n=lseek(g,0,SEEK_END);lseek(g,0,SEEK_SET);
- #define h0 "HTTP/1.1 200 OK\nContent-Length:"
- #define h1 "\nConnection:close\nContent-Type:"
- s=b;mc(s,h0,Z h0-1);s+=Z h0-1;s=su(s,n);mc(s,h1,Z h1-1);s+=Z h1-1;mc(s,m,mn(m));s+=mn(m);mc(s,"\n\n",2);s+=2;r=s-b;
+ I g=open(s,O_RDONLY);P(g<0,"not found")I n=lseek(g,0,SEEK_END);lseek(g,0,SEEK_SET);s=b;
+ ap(&s,"HTTP/1.1 200 OK\nContent-Length:");s=su(s,n);ap(&s,"\nConnection:close\nContent-Type:");ap(&s,m);ap(&s,"\n\n");r=s-b;
  W(r>0,wr(f,b,r);r=read(g,b,Z b))close(g);wrZ(1,"ok\n");(V*)0)
 #define port 8080
 I main(){struct sockaddr_in a;a.sin_family=AF_INET;a.sin_addr.s_addr=0;a.sin_port=htons(port);
