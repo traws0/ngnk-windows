1e69e768f80b0da6dd9cb3fbd9f1e304882a070e
ngn <@>
2020-11-29 21:03:36 +0200

    huf.c

--- huf/huf.c
+++ huf/huf.c
@@ -19,9 +19,9 @@ TD void V;TD char C;TD unsigned char UC;TD short H;TD unsigned short UH;TD int I
 S struct{C*s;I n;}a[256];S I na,f[512],c[512][2],r[256],nr; //f:freq,c:children,r:roots
 S V v(I x){c[x][0]>=0?o("["),v(c[x][0]),o(","),v(c[x][1]),o("]"):o("%d",x);}
 S I g()_(I u=0,x=*r,b=f[x];F(nr,Y(f[r[i]]<b,b=f[x=r[u=i]]))r[u]=r[--nr];x) //rm and return root with least freq
-I main(){I d=open("../g",0);C b[1024];
- W(1,I k=getdents(d,b,Z b),i=0;Y(k<=0,break)
-  W(i<k,struct{long _;I _1;UH l;C s[];}*e=(V*)(b+i);C*s=e->s;
+I main(){I d=open("../g",0);C b[1024];I k;
+ W((k=getdents(d,b,Z b))>0,I i=0;
+  W(i<k,struct{long _,_1;UH l;C s[];}*e=(V*)(b+i);C*s=e->s;
    Y(b[i+e->l-1]==DT_REG&&!strcmp(s+strlen(s)-2,".k"),
     I fd=openat(d,s,0),n=lseek(fd,0,SEEK_END);C*s=mmap(0,n,MAP_FILE|MAP_SHARED,PROT_READ,fd,0),*s0=s;close(fd);
     Y(*s=='/'&&s[1]=='h',W(*s&&*s-10,s++))a[na].s=s;a[na].n=n+s0-s;na++)
