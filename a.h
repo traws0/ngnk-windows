1291be4ebe6780b4190eff6c1508029127d563af
ngn <@>
2020-12-16 00:26:26 +0200

    .

--- a.h
+++ a.h
@@ -140,9 +140,9 @@ S OA au0=au(0),av0=av(0),au_out=au(25),au_cmd=au(28),au_plc=au(29),av_com=av(24)
 #define NH (H)(1<<15)
 #define WH (~NH)
 
-#define eH(x,y,z,u,n,...) n
-#define eF(f,x,a...) P(x,f(a))
-#define E_(t,a...) eF(eH(a,e##t##3,e##t##2,e##t##1,e##t##0),a)
+#define EN(x,y,z,u,n,...) n
+#define EF(f,x,a...) P(x,f(a))
+#define E_(t,a...) EF(EN(a,e##t##3,e##t##2,e##t##1,e##t##0),a)
 #define Ec(a...) E_(c,a)
 #define Ed(a...) E_(d,a)
 #define Ei(a...) E_(i,a)
@@ -155,9 +155,9 @@ S OA au0=au(0),av0=av(0),au_out=au(25),au_cmd=au(28),au_plc=au(29),av_com=av(24)
 #define Et(a...) E_(t,a)
 #define Ev(a...) E_(v,a)
 #define Ez(a...) E_(z,a)
-#define eA h(c,cpl)h(d,dom)h(i,idx)h(l,len)h(n,nyi)h(o,io)h(p,prs)h(r,rnk)h(s,stk)h(t,typ)h(v,val)h(z,lmt)
+#define EA h(c,cpl)h(d,dom)h(i,idx)h(l,len)h(n,nyi)h(o,io)h(p,prs)h(r,rnk)h(s,stk)h(t,typ)h(v,val)h(z,lmt)
 #define h(t,m) A0 e##t##0;A1 e##t##1;A2 e##t##2;A3 e##t##3;S A e##t##n(In,OA*a)_(eN(#m,n,a))
- eA
+ EA
 #undef h
 #define N_(r,a) ({A r_=(r);P(!r_,a;0)r_;}) //error pass-through
 #define N(r)      N_(r,)
--- e.c
+++ e.c
@@ -15,4 +15,4 @@ A1(epr,I h=64;C s0[3*h+Z(e.m)+6],*s=s0;*s++='\'';mc(s,e.m,e.n);s+=e.n;*s++=10;
 
 #define g(t,m,n,a...) NO A##n(e##t##n,e##n(#m,##a))
 #define h(a...) g(a,0)g(a,1,x)g(a,2,x,y)g(a,3,x,y,z)
-eA
+EA
