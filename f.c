e853d3a1e33c0b473625630c2f30b42a8990494c
ngn <@>
2021-01-04 20:57:29 +0200

    0N?0N

--- f.c
+++ f.c
@@ -3,7 +3,7 @@
 // ","/"abcd",'"=0x",/:+"0123456789abcdef"@(16#16)\2/|+4 64#(n{(|(8#2)\30)@2/'3'0,x,0}\n=!2*n)@'n:256
 S V rndl(L*r,UL n){S UL a=0xd5a986ae75c9a33b,b=0x1016d8e3483a8f0f,c=0x81f9e6260eb8e5df,d=0xfa9b718d8d0769bf;
  F(n,r[i]=a+d;O UL t=b<<17;c^=a;d^=b;b^=c;a^=d;c^=t;d=rot(d,45))}
-S A rnd(Ln,L m)_(Y(n==NL,n=-m*(m!=NL))Ax=aL(absL(n));rndl(xL,xn);Y(m,Y(m<=1ll<<32,F(xn,xl=(UI)xl*(UL)m>>32))E(F(xn,xl=(UL)xl%m)))
+S A rnd(Ln,L m)_(Y(n==NL,Ed(m<0)n=-m)Ax=aL(absL(n));rndl(xL,xn);Y(m,Y(m<=1ll<<32,F(xn,xl=(UI)xl*(UL)m>>32))E(F(xn,xl=(UL)xl%m)))
  Y(n<0,n=-n;El(n>m,x)F(n,Lk=m-n+i;Lv=((UL*)xL)[i]%=k+1;L*p=&xl;F(i,Y(xl==v,*p=k;B)))Ay=rnd(n,0);F(n,SWP(xl,xL[(UL)yl%(i+1)]))yr)x)
 SN I fAA(Ax/*0*/,Ay/*0*/)_(Q(xtA);F(xn,P(mtc_(xa,y),i))NI)
 I fpA(A*p,Ay)_(Ax=*p;In=xn,i=fAA(x,y);P(i<0,_q(*p,y);n)yr;i)
