0283b9fcbfa98860b4949da582875d2515916fb8
ngn <@>
2021-01-22 10:56:04 +0200

    .

--- web/k.js
+++ web/k.js
@@ -1,9 +1,11 @@
 'use strict';const BA='ngn/k, (c) 2019-2021 ngn, GNU AGPLv3 - https://git.sr.ht/~ngn/k/blob/master/LICENSE\n',
 T1=new TextEncoder,T0=new TextDecoder,t1=T1.encode.bind(T1),t0=T0.decode.bind(T0),
+z1=pako.inflate,z0=pako.deflate, //zlib
 u1=x=>chr(...t1(x)),u0=x=>t0(U8([...x].map(c=>c.ch(0)))), //utf8
 c1=x=>{let r='';for(let i=0;i<x.n;i++)r+=chr(x[i]);return r}, //8bit chars
 c0=x=>{const r=U8(x.n);for(let i=0;i<x.n;i++)r[i]=x.ch(i);return r},
-p1=x=>btoa(c1(pako.deflate(t1(x)))),p0=x=>{try{return t0(pako.inflate(c0(atob(x))))}catch(x){return''}}, //permalinks
+p1=x=>x?btoa(c1(pako.deflate(t1(x)))):'', //permalinks
+p0=x=>{try{return x?t0(pako.inflate(c0(atob(x)))):''}catch(x){return''}},
 {log,error}=console,{min,max}=Math,PR=' ',N='\n',U8=x=>new Uint8Array(x),chr=String.fromCharCode,
 Q=(s,f)=>s.split(',').map(f),cur=(ta,i)=>ta.setSelectionRange(i,i),ap=s=>{out.value+=s;cur(out,out.value.n)},
 skPR=i=>i+PR.n*(ed.value._(i,i+PR.n)===PR),upd=_=>mem=new DataView(app.memory.buffer),
@@ -54,18 +56,18 @@ if(location.hash==='#r'){document.body.classList.add('repl');wa(_=>app.init()) /
     ha[hi]=s._(p,q);inp=s._(skPR(p),q)+N;let l=ha.n-1;hi<l&&(ha[l]=ha[hi]);hi=ha.push('')-1;ap(q-s.n?inp+N:N);
     try{app.rep()}catch(x){ed.disabled=1;error(x)};ap(PR);return!1}}})}
 else{ //editor|output mode
- const ubc=_=>{let s=ed.value,i=s.io('\n\n'),r=[] //update and return byte count
+ const ubc=_=>{let s=ed.value,i=s.io('\n\n'),r=[] //update byte count and return the counted part
   if(i>=0){s=s._(0,i);r.push('till empty line')}
   if(s._(0,2)==='f:'){s=s._(2);r.push('not counting initial "f:"')}
-  bc.textContent=s.n+'bytes'+(r.n?`(${r.join(', ')})`:'');return s.n}
- rdy(_=>{const h=(location.hash||'')._(1);if(h)ed.value=p0(h)
+  bc.textContent=s.n+'bytes'+(r.n?`(${r.join(', ')})`:'');return s}
+ rdy(_=>{ed.value=p0(location.hash._(1))
   ev.onclick=_=>{wa(async _=>{ubc();location.hash=p1(ed.value)
    const g='golf.k';fs[g]=fs[g]||t1(await(await fetch(g)).text())
    const s='\\l golf.k\n'+ed.value;fs['a.k']=t1(s._(-1)===N?s:s+N);out.value=''
    const h=heap;heap+=T1.encodeInto('k\0a.k\0',M(heap,8)).written;const a=heap;S4(heap,[h,h+2,0,0]);heap+=16;
    const t=Date.now();try{app.main(2,a)}catch(e){if(e.message!=='exit(0)')throw e}tm.textContent=Date.now()-t})}
-  cg.onclick=_=>{const s=ed.value,h='ngn-'+hex8(hash(s))
-   out.value=`# [K (ngn/k)], ${ubc()} bytes\n\n    ${s.replace(/\n/g,'\n    ')}\n\n[Try it online!][${h}]\n`+
+  cg.onclick=_=>{const s=ed.value,h='ngn-'+hex8(hash(s)),g=ubc()
+   out.value=`# [K (ngn/k)], ${g.n} bytes\n\n    ${g.replace(/\n/g,'\n    ')}\n\n[Try it online!][${h}]\n`+
     `\n[ngn/k]: https://git.sr.ht/~ngn/k\n[${h}]: https://ngn.bitbucket.io/k#c${p1(s)}\n`}
   ed.onkeydown=x=>{const k=kc(x);if(k===1013){ev.onclick();return!1}if(k===1071){cg.onclick();return!1}}
   ed.onkeyup=thr(ubc,1000)
