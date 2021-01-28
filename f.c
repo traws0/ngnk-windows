e2836d6a0de781497a6f0617c13b86f04d607fa1
ngn <@>
2021-01-28 02:32:01 +0200

    f.c

--- f.c
+++ f.c
@@ -3,8 +3,9 @@
 // ","/"abcd",'"=0x",/:+"0123456789abcdef"@(16#16)\2/|+4 64#(n{(|(8#2)\30)@2/'3'0,x,0}\n=!2*n)@'n:256
 S V rndL(L*r,UL n){S UL a=0xd5a986ae75c9a33b,b=0x1016d8e3483a8f0f,c=0x81f9e6260eb8e5df,d=0xfa9b718d8d0769bf;
  i(n,r[i]=a+d;O UL t=b<<17;c^=a;d^=b;b^=c;a^=d;c^=t;d=rot(d,45))}
-S A rnd(Ln,Lm)_(Y(n==NL,Ed(m<0)n=-m)Ax=aL(absL(n));rndL(xL,xn);Y(m,Y(m<=1ll<<32,i(xn,xl=(UI)xl*(UL)m>>32))E(i(xn,xl=(UL)xl%m)))
- Y(n<0,n=-n;El(n>m,x)i(n,Lk=m-n+i;Lv=((UL*)xL)[i]%=k+1;L*p=&xl;j(i,Y(xl==v,*p=k;B)))Ay=rnd(n,0);m2(y,i(n,SWP(xl,xL[(UL)yl%(i+1)]))0))x)
+S A rnd(Ln,Lm)_(Y(n==NL,Ed(m<0)n=-m)
+ Au=aL(absL(n));rndL(uL,un);Y(m,Y(m<=1ll<<32,iul((UI)ul*(UL)m>>32))E(iul((UL)ul%m)))
+ Y(n<0,n=-n;El(n>m,u)i(n,Lk=m-n+i;Lv=((UL*)uL)[i]%=k+1;L*p=&ul;j(i,Y(ul==v,*p=k;B)))Ay=rnd(n,0);m2(y,i(n,SWP(ul,uL[(UL)yl%(i+1)]))0))u)
 S A rndD(Ln)_(A x=AT(tD,rnd(n,0));i(n,xl=xl&-1ull>>12|1023ll<<52)x)
 SN I fAA(Ax,Ay/*00*/)_(Q(xtA);i(xn,P(mtc_(xa,y),i))NI)
 I fpA(A*p,Ay/*1*/)_(Ax=*p;In=xn,i=fAA(x,y);P(i<0,_q(*p,y);n)y(0);i)
@@ -18,5 +19,5 @@ A2(fnd,Xs(SF(x,y,"``j`hex`aes",des,jsr,unh,aes1))Xm(Az=kv(&x);idx(x,Nx(fnd(z,y))
  Xz(Yz(rnd(gl(gL(x)),gl(y)))Yc(K("{`c$\"aA\"[y~\"A\"]+x?26}",x,y))Yd(mul(sub(rndD(gl(gL(x))),ad(1)),y))K("{y@x?#y}",x,y))
  Ymt(fir(N(fnd(x,enl(y)))))
  P(xtS&&ytS,fnd(AT(tI,x),AT(tI,y)))
- P(xtZ&&ytZ,N(sup(&x,&y));Au=aL(yn);TY(fC)*f=CH(Zt[xt],&fC,fH,fI,fL);i(un,ul=NNL(f(xC,xn,yC+i*ZT[xt])))x(y(u)))
- Au=aL(yN);i(un,Az=get(y,i);ul=NNL(fnd1(x,z));z(0))x(y(u)))
+ P(xtZ&&ytZ,N(sup(&x,&y));Au=aL(yn);TY(fC)*f=CH(Zt[xt],&fC,fH,fI,fL);x(y(iul(NNL(f(xC,xn,yC+i*ZT[xt]))))))
+ Au=aL(yN);x(y(iul(Az=get(y,i);dex(z,NNL(fnd1(x,z)))))))
