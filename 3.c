0bdfce43995d5017cb33ee01a9925f3e145144d5
ngn <@>
2021-02-01 18:01:02 +0200

    3.c

--- 3.c
+++ 3.c
@@ -1,23 +1,23 @@
 #include"a.h" // ngn/k, (c) 2019-2021 ngn, GNU AGPLv3 - https://git.sr.ht/~ngn/k/blob/master/LICENSE
-#define v2(f,T,e...) S L f(OV*RE a0,OV*RE b0,V*RE r0,Ln)_(O T*a=a0,*b=b0;T*r=r0;LN(b)LN(r)e)
-#define V2(f,T,e...) v2(f,T,LN(a);e)
+#define f0(f,T,e...) S L f(OV*RE a0,OV*RE b0,V*RE r0,Ln)_(O T*a=a0,*b=b0;T*r=r0;LN(b)LN(r)e)
+#define f1(f,T,e...) f0(f,T,LN(a);e)
 
-#define h0(f,T,op) v2(f,T,i(PD(n,a),r[i]=*a   op b[i])n) //atom-list, no overflows
-#define h1(f,T,op) V2(f,T,i(PD(n,a),r[i]=a[i] op b[i])n) //list-list, no overflows
+#define h0(f,T,op) f0(f,T,i(PD(n,a),r[i]=*a   op b[i])n) //atom-list, no overflows
+#define h1(f,T,op) f1(f,T,i(PD(n,a),r[i]=a[i] op b[i])n) //list-list, no overflows
 h0(alL,L,+)h1(aLL,L,+)h0(mlL,L,*)h1(mLL,L,*)h0(adD,D,+)h1(aDD,D,+)h0(mdD,D,*)h1(mDD,D,*)
 
 #define g (32/Z(H))
-v2(ahH,H,Li=0;W(i<n,j(g,r[i+j]=*a    +b[i+j])i+=g)i)
-v2(mhH,H,Li=0;W(i<n,j(g,r[i+j]=*a    *b[i+j])i+=g)i)
-V2(aHH,H,Li=0;W(i<n,j(g,r[i+j]=a[i+j]+b[i+j])i+=g)n)
-V2(mHH,H,Li=0;W(i<n,j(g,r[i+j]=a[i+j]*b[i+j])i+=g)n)
+f0(ahH,H,Li=0;W(i<n,j(g,r[i+j]=*a    +b[i+j])i+=g)i)
+f0(mhH,H,Li=0;W(i<n,j(g,r[i+j]=*a    *b[i+j])i+=g)i)
+f1(aHH,H,Li=0;W(i<n,j(g,r[i+j]=a[i+j]+b[i+j])i+=g)n)
+f1(mHH,H,Li=0;W(i<n,j(g,r[i+j]=a[i+j]*b[i+j])i+=g)n)
 #undef g
 
 #define g (32/Z(I))
-v2(aiI,I,Li=0;W(i<n,j(g,r[i+j]=*a    +b[i+j])i+=g)i)
-v2(miI,I,Li=0;W(i<n,j(g,r[i+j]=*a    *b[i+j])i+=g)i)
-V2(aII,I,Li=0;W(i<n,j(g,r[i+j]=a[i+j]+b[i+j])i+=g)n)
-V2(mII,I,Li=0;W(i<n,j(g,r[i+j]=a[i+j]*b[i+j])i+=g)n)
+f0(aiI,I,Li=0;W(i<n,j(g,r[i+j]=*a    +b[i+j])i+=g)i)
+f0(miI,I,Li=0;W(i<n,j(g,r[i+j]=*a    *b[i+j])i+=g)i)
+f1(aII,I,Li=0;W(i<n,j(g,r[i+j]=a[i+j]+b[i+j])i+=g)n)
+f1(mII,I,Li=0;W(i<n,j(g,r[i+j]=a[i+j]*b[i+j])i+=g)n)
 #undef g
 
 A2(add,P(xtmMA||ytmMA,eac(av(1),A(x,y),2))N(sup(&x,&y));Ik=xtT<<1|ytT;P(!k,Xd(ad(gd(x)+gd(y)))az(gl(x)+gl(y)))
