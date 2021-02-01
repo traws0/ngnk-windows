8f05b5605b992159682e86c0b8c8e81a6abf9d0d
ngn <@>
2021-02-01 16:46:24 +0200

    3.c

--- 3.c
+++ 3.c
@@ -1,26 +1,28 @@
 #include"a.h" // ngn/k, (c) 2019-2021 ngn, GNU AGPLv3 - https://git.sr.ht/~ngn/k/blob/master/LICENSE
-#define V2(f,T,e...) S I f(OV*RE a0,OV*RE b0,V*RE r0,UL n)_(O T*a=a0,*b=b0;T*r=r0;LN(b)LN(r)n=PD(n,a);e)
+#define v2(f,T,e...) S L f(OV*RE a0,OV*RE b0,V*RE r0,Ln)_(O T*a=a0,*b=b0;T*r=r0;LN(b)LN(r)e)
+#define V2(f,T,e...) v2(f,T,LN(a);e)
 
-V2(ahH,H,Hv=*a;i(n,r[i]=   v+b[i])0)
-V2(aiI,I,Iv=*a;i(n,r[i]=   v+b[i])0)
-V2(alL,L,Lv=*a;i(n,r[i]=   v+b[i])0)
-V2(adD,D,Dv=*a;i(n,r[i]=   v+b[i])0)
-V2(aHH,H,LN(b);i(n,r[i]=a[i]+b[i])0)
-V2(aII,I,LN(b);i(n,r[i]=a[i]+b[i])0)
-V2(aLL,L,LN(b);i(n,r[i]=a[i]+b[i])0)
-V2(aDD,D,LN(b);i(n,r[i]=a[i]+b[i])0)
+#define h0(f,T,op) v2(f,T,i(PD(n,a),r[i]=*a   op b[i])n) //atom-list, no overflows
+#define h1(f,T,op) V2(f,T,i(PD(n,a),r[i]=a[i] op b[i])n) //list-list, no overflows
+h0(alL,L,+)h1(aLL,L,+)h0(mlL,L,*)h1(mLL,L,*)h0(adD,D,+)h1(aDD,D,+)h0(mdD,D,*)h1(mDD,D,*)
+
+#define g (32/Z(H))
+v2(ahH,H,Li=0;W(i<n,j(g,r[i+j]=*a    +b[i+j])i+=g)i)
+v2(mhH,H,Li=0;W(i<n,j(g,r[i+j]=*a    *b[i+j])i+=g)i)
+V2(aHH,H,Li=0;W(i<n,j(g,r[i+j]=a[i+j]+b[i+j])i+=g)n)
+V2(mHH,H,Li=0;W(i<n,j(g,r[i+j]=a[i+j]*b[i+j])i+=g)n)
+#undef g
+
+#define g (32/Z(I))
+v2(aiI,I,Li=0;W(i<n,j(g,r[i+j]=*a    +b[i+j])i+=g)i)
+v2(miI,I,Li=0;W(i<n,j(g,r[i+j]=*a    *b[i+j])i+=g)i)
+V2(aII,I,Li=0;W(i<n,j(g,r[i+j]=a[i+j]+b[i+j])i+=g)n)
+V2(mII,I,Li=0;W(i<n,j(g,r[i+j]=a[i+j]*b[i+j])i+=g)n)
+#undef g
+
 A2(add,P(xtmMA||ytmMA,eac(av(1),A(x,y),2))N(sup(&x,&y));Ik=xtT<<1|ytT;P(!k,Xd(ad(gd(x)+gd(y)))az(gl(x)+gl(y)))
  Y(k==2||(k==3&&xr==1),SWP(x,y))El(k==3&&xn-yn,x,y)
  Au=yr-1?atn(yt,yn):y;CH(yt-tH+4*(k==3),&ahH,aiI,alL,adD,aHH,aII,aLL,aDD)(xtP?(V*)&x:xC,yC,uC,un);x(y-u?y(u):u))
-
-V2(mhH,H,Hv=*a;i(n,r[i]=   v*b[i])0)
-V2(miI,I,Iv=*a;i(n,r[i]=   v*b[i])0)
-V2(mlL,L,Lv=*a;i(n,r[i]=   v*b[i])0)
-V2(mdD,D,Dv=*a;i(n,r[i]=   v*b[i])0)
-V2(mHH,H,LN(b);i(n,r[i]=a[i]*b[i])0)
-V2(mII,I,LN(b);i(n,r[i]=a[i]*b[i])0)
-V2(mLL,L,LN(b);i(n,r[i]=a[i]*b[i])0)
-V2(mDD,D,LN(b);i(n,r[i]=a[i]*b[i])0)
 A2(mul,P(xtmMA||ytmMA,eac(av(3),A(x,y),2))N(sup(&x,&y));Ik=xtT<<1|ytT;P(!k,Xd(ad(gd(x)*gd(y)))az(gl(x)*gl(y)))
  Y(k==2||(k==3&&xr==1),SWP(x,y))El(k==3&&xn-yn,x,y)
  Au=yr-1?atn(yt,yn):y;CH(yt-tH+4*(k==3),&mhH,miI,mlL,mdD,mHH,mII,mLL,mDD)(xtP?(V*)&x:xC,yC,uC,un);x(y-u?y(u):u))
