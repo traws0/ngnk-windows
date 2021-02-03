e4f7aeb31efe0f9c29701713e8493b8c3e34f9cb
ngn <@>
2021-02-03 23:32:57 +0200

    3.c

--- 3.c
+++ 3.c
@@ -5,15 +5,19 @@
 #define h1(f,T,op) f1(f,T,i(PD(n,a),r[i]=a[i] op b[i])n) //list op list, no overflows
 h0(alL,L,+)h1(aLL,L,+)h0(mlL,L,*)h1(mLL,L,*)h0(adD,D,+)h1(aDD,D,+)h0(mdD,D,*)h1(mDD,D,*)
 #define G(T) (32/Z(T))
-#define g0(f,T,T1,op) f0(f,T,Li=0;W(i<n,T1 v[G(T)];j(G(T),v[j]=a[0]op b[j])j(G(T),P(v[j]<N##T||W##T<v[j],i))j(G(T),r[j]=v[j])i+=G(T);        b+=G(T);r+=G(T))i)
-#define g1(f,T,T1,op) f1(f,T,Li=0;W(i<n,T1 v[G(T)];j(G(T),v[j]=a[j]op b[j])j(G(T),P(v[j]<N##T||W##T<v[j],i))j(G(T),r[j]=v[j])i+=G(T);a+=G(T);b+=G(T);r+=G(T))i)
+#define g0(f,T,T1,op) f0(f,T,Li=0;W(i<n,T1 v[G(T)];j(G(T),v[j]=(T1)a[0]op(T1)b[j])j(G(T),P(v[j]<N##T||W##T<v[j],i))j(G(T),r[j]=v[j])i+=G(T);        b+=G(T);r+=G(T))i)
+#define g1(f,T,T1,op) f1(f,T,Li=0;W(i<n,T1 v[G(T)];j(G(T),v[j]=(T1)a[j]op(T1)b[j])j(G(T),P(v[j]<N##T||W##T<v[j],i))j(G(T),r[j]=v[j])i+=G(T);a+=G(T);b+=G(T);r+=G(T))i)
 g0(ahH,H,I,+)g0(aiI,I,L,+)g0(mhH,H,I,*)g0(miI,I,L,*)g1(aHH,H,I,+)g1(aII,I,L,+)g1(mHH,H,I,*)g1(mII,I,L,*)
 
+S A wdn(Ax,Li,Lj,Ln)_(Ct=xt;Q(c3(tC,t,tI));Au=atn(t+1,n);wf[t-tC](xC+ZT[t]*i,uC,j-i);x(u))
 SN A adm(Ax,Ay,Iv)_(P(xtmMA||ytmMA,eac(av(v?3:1),A(x,y),2))N(sup(&x,&y));Ik=xtT<<1|ytT;
- P(!k,Xd(ad(v?gd(x)*gd(y):gd(x)+gd(y)))az(v?gl(x)*gl(y):gl(x)+gl(y)))Y(k==2||(k==3&&xr==1),SWP(x,y))El(k==3&&xn-yn,x,y)
- Au=yr-1?atn(yt,yn):y;
- Lm=CH(yt-tH+4*(k==3)+8*v,&ahH,aiI,alL,adD,aHH,aII,aLL,aDD,mhH,miI,mlL,mdD,mHH,mII,mLL,mDD)(xtP?(V*)&x:xC,yC,uC,un);
- Y(m<yn,write(1,"OVERFLOW\n",9);exit(1))
+ P(!k,Xd(ad(v?gd(x)*gd(y):gd(x)+gd(y)))az(v?gl(x)*gl(y):gl(x)+gl(y)))Y(k==2||(k==3&&xr==1),SWP(x,y))
+ S TY(&ahH)f[]={ahH,aiI,alL,adD,aHH,aII,aLL,aDD,mhH,miI,mlL,mdD,mHH,mII,mLL,mDD};
+ Ln=yn;El(k==3&&xn-n,x,y)Au=yr-1?atn(yt,n):y;Lm=f[yt-tH+4*(k==3)+8*v](xtP?(V*)&x:xC,yC,uC,un);
+ Y(m<yn,
+  Y(xtt,x=CH(xt-tc,&gH,gI,gL)(x))E(x=wdn(x,m,xn,xn-m))
+  Y(y-u,y=wdn(y,m,yn,yn-m);u=wdn(u,0,m,un))E(y=u=wdn(u,0,un,un))
+  L mw=m*ZT[ut];f[yt-tH+4*(k==3)+8*v](xtP?(V*)&x:xC,yC+(y-u?0:mw),uC+mw,un))
  x(y-u?y(u):u))
 A2(add,adm(x,y,0))
 A2(mul,adm(x,y,1))
