af4ee40e64aa1fe5c879074d5d8d5fd51d40e5e1
ngn <@>
2021-02-06 01:06:20 +0200

    3.c

--- 3.c
+++ 3.c
@@ -1,23 +1,39 @@
 #include"a.h" // ngn/k, (c) 2019-2021 ngn, GNU AGPLv3 - https://git.sr.ht/~ngn/k/blob/master/LICENSE
-#define f0(f,T,e...) S L f(OV*RE a0,OV*RE b0,V*RE r0,Ln)_(O T*a=a0,*b=b0;T*r=r0;LN(b)LN(r)e)
-#define f1(f,T,e...) f0(f,T,LN(a);e)
-#define h0(f,T,op) f0(f,T,i(PD(n,a),r[i]=a[0] op b[i])n) //atom op list, no overflows
-#define h1(f,T,op) f1(f,T,i(PD(n,a),r[i]=a[i] op b[i])n) //list op list, no overflows
+#define h(f,T,e...) S L f(OV*RE a0,OV*RE b0,V*RE r0,Ln)_(O T*a=a0,*b=b0;T*r=r0;LN(b)LN(r)e)
+#define h0(f,T,o) h(f,T,      i(PD(n,a),r[i]=a[0]o b[i])n) //atom-list, no overflows
+#define h1(f,T,o) h(f,T,LN(a);i(PD(n,a),r[i]=a[i]o b[i])n) //list-list, no overflows
 h0(alL,L,+)h1(aLL,L,+)h0(mlL,L,*)h1(mLL,L,*)h0(adD,D,+)h1(aDD,D,+)h0(mdD,D,*)h1(mDD,D,*)
+#define IF(x,y...) IF##x(y)
+#define IF0(x)
+#define IF1(x) x
 #define G(T) (32/Z(T))
-#define g0(f,T,T1,op) f0(f,T,Li=0;W(i<n,T1 v[G(T)];j(G(T),v[j]=(T1)a[0]op(T1)b[j])j(G(T),P(v[j]<N##T||W##T<v[j],i))j(G(T),r[j]=v[j])i+=G(T);        b+=G(T);r+=G(T))i)
-#define g1(f,T,T1,op) f1(f,T,Li=0;W(i<n,T1 v[G(T)];j(G(T),v[j]=(T1)a[j]op(T1)b[j])j(G(T),P(v[j]<N##T||W##T<v[j],i))j(G(T),r[j]=v[j])i+=G(T);a+=G(T);b+=G(T);r+=G(T))i)
-g0(ahH,H,I,+)g0(aiI,I,L,+)g0(mhH,H,I,*)g0(miI,I,L,*)g1(aHH,H,I,+)g1(aII,I,L,+)g1(mHH,H,I,*)g1(mII,I,L,*)
+#define ha(l,f,T,R)\
+ h(f,T,IF(l,LN(a));T v[G(T)];Li=0;\
+  W(i<n,\
+   j(G(T),v[j]=a[IF(l,j+)0]+b[j])\
+   T o=0;j(G(T),o|=(a[IF(l,j+)0]^v[j])&(b[j]^v[j]))P(o>>Z(T)*8-1,i)\
+   j(G(T),r[j]=v[j])\
+   i+=G(T);IF(l,a+=G(T));b+=G(T);r+=G(T))\
+  i)
+#define hm(l,f,T,R)\
+ h(f,T,IF(l,LN(a));R v[G(T)];Li=0;\
+  W(i<n,\
+   j(G(T),v[j]=a[IF(l,j+)0]*(R)b[j])\
+   j(G(T),P(v[j]<N##T||W##T<v[j],i))\
+   j(G(T),r[j]=v[j])\
+   i+=G(T);IF(l,a+=G(T));b+=G(T);r+=G(T))\
+  i)
+ha(0,ahH,H,I)ha(0,aiI,I,L)ha(1,aHH,H,I)ha(1,aII,I,L) //+
+hm(0,mhH,H,I)hm(0,miI,I,L)hm(1,mHH,H,I)hm(1,mII,I,L) //*
 
-S A wdn(Ax,Li,Lj,Ln)_(Ct=xt;Q(c3(tC,t,tI));Au=atn(t+1,n);wf[t-tC](xC+ZT[t]*i,uC,j-i);x(u))
-SN A adm(Ax,Ay,Iv)_(P(xtmMA||ytmMA,eac(av(v?3:1),A(x,y),2))N(sup(&x,&y));Ik=xtT<<1|ytT;
+S A w(Ax,Li,Lj,Ln)_(Ct=xt;Q(c3(tC,t,tI));Au=atn(t+1,n);wf[t-tC](xC+ZT[t]*i,uC,j-i);x(u)) //widen
+SN A f(Ax,Ay,Iv)_(P(xtmMA||ytmMA,eac(av(v?3:1),A(x,y),2))N(sup(&x,&y));Ik=xtT<<1|ytT;
  P(!k,Xd(ad(v?gd(x)*gd(y):gd(x)+gd(y)))az(v?gl(x)*gl(y):gl(x)+gl(y)))Y(k==2||(k==3&&xr==1),SWP(x,y))
  S TY(&ahH)f[]={ahH,aiI,alL,adD,aHH,aII,aLL,aDD,mhH,miI,mlL,mdD,mHH,mII,mLL,mDD};
  Ln=yn;El(k==3&&xn-n,x,y)Au=yr-1?atn(yt,n):y;Lm=f[yt-tH+4*(k==3)+8*v](xtP?(V*)&x:xC,yC,uC,n);
  Y(m<n,
-  Y(k-3,x=CH(xt-tc,&gH,gI,gL)(x))E(x=wdn(x,m,n,n-m))
-  Y(y-u,y=wdn(y,m,n,n-m);u=wdn(u,0,m,n))E(y=u=wdn(u,0,n,n))
+  Y(k-3,x=CH(xt-tc,&gH,gI,gL)(x))E(x=w(x,m,n,n-m))
+  Y(y-u,y=w(y,m,n,n-m);u=w(u,0,m,n))E(y=u=w(u,0,n,n))
   L mw=m*ZT[ut];f[yt-tH+4*(k==3)+8*v](xtP?(V*)&x:xC,yC+(y-u?0:mw),uC+mw,n-m))
  x(y-u?y(u):u))
-A2(add,adm(x,y,0))
-A2(mul,adm(x,y,1))
+A2(add,f(x,y,0))A2(mul,f(x,y,1))
