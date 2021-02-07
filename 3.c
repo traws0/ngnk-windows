b35469f9614007908edb1f8cf1aabdc9e08c5c16
ngn <@>
2021-02-07 22:48:01 +0200

    3.c

--- 3.c
+++ 3.c
@@ -1,28 +1,13 @@
 #include"a.h" // ngn/k, (c) 2019-2021 ngn, GNU AGPLv3 - https://git.sr.ht/~ngn/k/blob/master/LICENSE
-#define IF(x,y...) IF##x(y)
-#define IF0(x)
-#define IF1(x) x
-
 #define h(f,T,e...) S L f(OV*RE a0,OV*RE b0,V*RE r0,Ln)_(O T*a=a0,*b=b0;T*r=r0;LN(b)LN(r)e)
-#define hn(l,f,T,o) h(f,T,IF(l,LN(a));i(PD(n,a),r[i]=a[IF(l,i+)0]o b[i])n)
+#define hn(l,f,T,o) h(f,T,Y(l,LN(a))i(PD(n,a),r[i]=a[l*i]o b[i])n) //+ and * for widest types, no overflow
 hn(0,alL,L,+)hn(1,aLL,L,+)hn(0,mlL,L,*)hn(1,mLL,L,*)hn(0,adD,D,+)hn(1,aDD,D,+)hn(0,mdD,D,*)hn(1,mDD,D,*)
 
 #define G(T) (32/Z(T))
-#define ha(l,f,T,R) h(f,T,IF(l,LN(a));T v[G(T)];Li=0;\
-  W(i<n,\
-   j(G(T),v[j]=a[IF(l,j+)0]+b[j])\
-   T o=0;j(G(T),o|=(a[IF(l,j+)0]^v[j])&(b[j]^v[j]))P(o>>Z(T)*8-1,i)\
-   j(G(T),r[j]=v[j])\
-   i+=G(T);IF(l,a+=G(T));b+=G(T);r+=G(T))\
-  i)
-#define hm(l,f,T,R) h(f,T,IF(l,LN(a));R v[G(T)];Li=0;\
-  W(i<n,\
-   j(G(T),v[j]=a[IF(l,j+)0]*(R)b[j])\
-   j(G(T),P(v[j]<N##T||W##T<v[j],i))\
-   j(G(T),r[j]=v[j])\
-   i+=G(T);IF(l,a+=G(T));b+=G(T);r+=G(T))\
-  i)
-ha(0,ahH,H,I)ha(0,aiI,I,L)ha(1,aHH,H,I)ha(1,aII,I,L) //+
+#define ho(l,f,T,e...) h(f,T,Y(l,LN(a))Li=0;W(i<n,e;j(G(T),r[j]=v[j])i+=G(T);a+=l*G(T);b+=G(T);r+=G(T))i)
+#define ha(l,f,T)   ho(l,f,T,T v[G(T)];j(G(T),v[j]=a[l*j]+b[j])T o=0;j(G(T),o|=(a[l*j]^v[j])&(b[j]^v[j]))P(o>>Z(T)*8-1,i))
+#define hm(l,f,T,R) ho(l,f,T,R v[G(T)];j(G(T),v[j]=a[l*j]*(R)b[j])j(G(T),P(v[j]<=N##T||W##T<=v[j],i)))
+ha(0,ahH,H  )ha(0,aiI,I  )ha(1,aHH,H  )ha(1,aII,I  ) //+
 hm(0,mhH,H,I)hm(0,miI,I,L)hm(1,mHH,H,I)hm(1,mII,I,L) //*
 
 S A w(Ax,Li,Lj,Ln)_(Ct=xt;Q(c3(tC,t,tI));Au=atn(t+1,n);wf[t-tC](xC+ZT[t]*i,uC,j-i);x(u)) //widen
