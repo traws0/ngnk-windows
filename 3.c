5592441fb0a0e768627c6154d2effe87dccc3d1a
ngn <@>
2021-02-11 14:31:58 +0200

    3.c

--- 3.c
+++ 3.c
@@ -1,17 +1,13 @@
 #include"a.h" // ngn/k, (c) 2019-2021 ngn, GNU AGPLv3 - https://git.sr.ht/~ngn/k/blob/master/LICENSE
-#define h(f,T,e...) S L f(OV*RE a0,OV*RE b0,V*RE r0,Ln)_(O T*a=a0,*b=b0;T*r=r0;AL(b)AL(r)e)
-#define hn(l,f,T,o) h(f,T,Y(l,AL(a))i(PD(n,a),r[i]=a[l*i]o b[i])n) //+ and * for widest types, no overflow
-hn(0,alL,L,+)hn(1,aLL,L,+)hn(0,mlL,L,*)hn(1,mLL,L,*)hn(0,adD,D,+)hn(1,aDD,D,+)hn(0,mdD,D,*)hn(1,mDD,D,*)
-
-#define G(T) (32/Z(T))
-#define ho(l,f,T,g,e...) h(f,T,Y(l,AL(a))Li=0;W(i<n,e;j(g,r[j]=v[j])i+=g;a+=l*g;b+=g;r+=g)i)
-#define hA(l,f,T,g)      ho(l,f,T,g,T v[g];j(g,v[j]=a[l*j]+b[j])T o=0;j(g,o|=(a[l*j]^v[j])&(b[j]^v[j]))P(o>>Z(T)*8-1,i))
-#define hM(l,f,T,R,g)    ho(l,f,T,g,R v[g];j(g,v[j]=a[l*j]*(R)b[j])j(g,P(v[j]<=N##T||W##T<=v[j],i)))
-#define ha(l,f,T)        hA(l,f,T,  G(T))
-#define hm(l,f,T,R)      hM(l,f,T,R,G(T))
-ha(0,ahH,H  )ha(0,aiI,I  )ha(1,aHH,H  )ha(1,aII,I  ) //+
-hm(0,mhH,H,I)hm(0,miI,I,L)hm(1,mHH,H,I)hm(1,mII,I,L) //*
-
+#define  h(f,T,e...)      S L f(OV*RE a0,OV*RE b0,V*RE r0,Ln)_(O T*a=a0,*b=b0;T*r=r0;AL(b)AL(r)e)
+#define hn(f,l,T,o)      h(f,T,Y(l,AL(a))i(PD(n,a),r[i]=a[l*i]o b[i])n) //+ and * for widest types, no overflow
+#define ho(f,l,T,g,e...) h(f,T,Y(l,AL(a))Li=0;W(i<n,e;j(g,r[j]=v[j])i+=g;a+=l*g;b+=g;r+=g)i)
+#define hA(f,l,T,g)      ho(f,l,T,g,T v[g];j(g,v[j]=a[l*j]+b[j])T o=0;j(g,o|=(a[l*j]^v[j])&(b[j]^v[j]))P(o>>Z(T)*8-1,i))
+#define hM(f,l,T,R,g)    ho(f,l,T,g,R v[g];j(g,v[j]=a[l*j]*(R)b[j])j(g,P(v[j]<=N##T||W##T<=v[j],i)))
+#define ha(f,l,T)        hA(f,l,T,  (32/Z(T)))
+#define hm(f,l,T,R)      hM(f,l,T,R,(32/Z(T)))
+ha(ahH,0,H  )ha(aiI,0,I  )ha(aHH,1,H  )ha(aII,1,I  )hn(alL,0,L,+)hn(aLL,1,L,+)hn(adD,0,D,+)hn(aDD,1,D,+)
+hm(mhH,0,H,I)hm(miI,0,I,L)hm(mHH,1,H,I)hm(mII,1,I,L)hn(mlL,0,L,*)hn(mLL,1,L,*)hn(mdD,0,D,*)hn(mDD,1,D,*)
 S A f(Ax,Ay,Iv)_(P(xtmMA||ytmMA,eac(av(v?3:1),A(x,y),2))N(sup(&x,&y));Ik=xtT<<1|ytT;
  P(!k,Xd(ad(v?gd(x)*gd(y):gd(x)+gd(y)))az(v?gl(x)*gl(y):gl(x)+gl(y)))Y(k==2||(k==3&&xr==1),SWP(x,y))
  S TY(&ahH)f[][2][4]={{{ahH,aiI,alL,adD},{aHH,aII,aLL,aDD}},{{mhH,miI,mlL,mdD},{mHH,mII,mLL,mDD}}};
