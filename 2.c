0b73ad18cee877e154912182b82c0c3d79ca39f1
ngn <@>
2021-03-07 11:44:04 +0200

    2.c

--- 2.c
+++ 2.c
@@ -1,39 +1,35 @@
 #include"a.h" // ngn/k, (c) 2019-2021 ngn, GNU AGPLv3 - https://git.sr.ht/~ngn/k/blob/master/LICENSE
 S L cD(Dd)_(Lv=*(L*)&d;v^(UL)(v>>63)>>1) //comparable double
-#define hh(v,fb,fh,fi,fl,fd) S B v##b(B x,B y)_(fb) S H v##h(H x,H y)_(fh) S I v##i(I x,I y)_(fi) S L v##l(L x,L y)_(fl) S D v##d(D x,D y)_(fd)
-#define hd(v,f_,fd) hh(v,f_,f_,f_,f_,fd)
+#define hf(v,T,f) S T v(T x,T y)_(f)
+#define hh(v,fb,fh,fi,fl,fd) hf(v##b,B,fb)hf(v##h,H,fh)hf(v##i,I,fi)hf(v##l,L,fl)hf(v##d,D,fd)
+#define hd(v,f,g) hh(v,f,f,f,f,g)
 #define h(v,f) hd(v,f,f)
 h(dex,y)h(add,x+y)h(sub,x-y)h(mul,x*y)hd(dvd,y?x/y:0,x/y)hd(mod,x>0?(y%x+x)%x:x?y/-x:y,y-(L)(y/x)*x)
 h(mnm,min(x,y))h(mxm,max(x,y))hd(ltn,x<y,cD(x)<cD(y))hd(gtn,x>y,cD(x)>cD(y))hd(eql,x==y,*(L*)&x==*(L*)&y)
 #undef h
 
-#define ht(v,t,T,tT,Tt,TT,R) ht0(v,t,T,R)\
- SN I v##t##t(T*RE a,T*RE b,R*RE r,UL n)_(*r=v##t(*a,*b);0)\
- SN I v##t##T(T*RE a,T*RE b,R*RE r,UL n)tT\
- SN I v##T##t(T*RE a,T*RE b,R*RE r,UL n)Tt\
- SN I v##T##T(T*RE a,T*RE b,R*RE r,UL n)TT
 #define ht0(v,t,T,R)\
  SN I v##T##s(R*RE a,T*RE b,R*RE r,UL n)_(R c=*a;i(PD(n,b),r[i]=c=v##t(c,b[i]))            0)\
  SN I v##T##p(T*RE a,T*RE b,R*RE r,UL n)_(T c=*a;i(PD(n,b),T d=b[i];r[i]=v##t(b[i],c);c=d);0)\
  SN I v##T##a(T*RE a,UL n,L*RE p,L m,T*RE b,I d)_(i(m,Lj=p[i];P(!in(j,n),0)a[j]=v##t(a[j],b[d*i]))0)
-#define v(v,bB,Bb,BB,hH,Hh,HH,iI,Ii,II,lL,Ll,LL,dD,Dd,DD,rl,rd) \
- ht(v,b,B,bB,Bb,BB,B)ht(v,h,H,hH,Hh,HH,H)ht(v,i,I,iI,Ii,II,I)ht(v,l,L,lL,Ll,LL,rl)ht(v,d,D,dD,Dd,DD,rd)
 #define v0(v,rl,rd) ht0(v,b,B,B)ht0(v,h,H,H)ht0(v,i,I,I)ht0(v,l,L,rl)ht0(v,d,D,rd)
-#define tT(f) _(AL(b)AL(r)TY(*a)c=*a;i(PD(n,a),*r++=f(c,*b++))0)
+#define ht(v,t,T,Tt,R) ht0(v,t,T,R)\
+ SN I v##t##t(T*RE a,T*RE b,R*RE r,UL n)_(*r=v##t(*a,*b);0)\
+ SN I v##t##T(T*RE a,T*RE b,R*RE r,UL n)_(AL(b)AL(r)TY(*a)c=*a;i(PD(n,a),*r++=v##t(c,*b++))0)\
+ SN I v##T##t(T*RE a,T*RE b,R*RE r,UL n)Tt\
+ SN I v##T##T(T*RE a,T*RE b,R*RE r,UL n)_(AL(a)AL(b)AL(r)i(PD(n,a),*r++=v##t(*a++,*b++))0)
+#define v(v,Bb,Hh,Ii,Ll,Dd,rl,rd) ht(v,b,B,Bb,B)ht(v,h,H,Hh,H)ht(v,i,I,Ii,I)ht(v,l,L,Ll,rl)ht(v,d,D,Dd,rd)
 #define Tt(f) _(AL(a)AL(r)TY(*b)c=*b;i(PD(n,a),*r++=f(*a++,c))0)
-#define TT(f) _(AL(a)AL(b)AL(r)      i(PD(n,a),*r++=f(*a++,*b++))0)
 #define s(x) _(x(b,a,r,n))
-#define n(x) _(TY(*b)c=-*b;x(a,&c,r,n))
-//    bB       Bb       BB       hH       Hh       HH       iI       Ii       II       lL       Ll       LL       dD       Dd       DD
-v(dvd,tT(dvdb),Tt(dvdb),TT(dvdb),tT(dvdh),Tt(dvdh),TT(dvdh),tT(dvdi),Tt(dvdi),TT(dvdi),tT(dvdl),Tt(dvdl),TT(dvdl),tT(dvdd),Tt(dvdd),TT(dvdd),L,D) // % 4
-v(mod,tT(modb),Tt(modb),TT(modb),tT(modh),Tt(modh),TT(modh),tT(modi),Tt(modi),TT(modi),tT(modl),Tt(modl),TT(modl),tT(modd),Tt(modd),TT(modd),L,D) // ! 5
-v(mnm,tT(mnmb),s(mnmbB),TT(mnmb),tT(mnmh),s(mnmhH),TT(mnmh),tT(mnmi),s(mnmiI),TT(mnmi),tT(mnml),s(mnmlL),TT(mnml),tT(mnmd),s(mnmdD),TT(mnmd),L,D) // & 6
-v(mxm,tT(mxmb),s(mxmbB),TT(mxmb),tT(mxmh),s(mxmhH),TT(mxmh),tT(mxmi),s(mxmiI),TT(mxmi),tT(mxml),s(mxmlL),TT(mxml),tT(mxmd),s(mxmdD),TT(mxmd),L,D) // | 7
-v(ltn,tT(ltnb),Tt(ltnb),TT(ltnb),tT(ltnh),Tt(ltnh),TT(ltnh),tT(ltni),Tt(ltni),TT(ltni),tT(ltnl),Tt(ltnl),TT(ltnl),tT(ltnd),Tt(ltnd),TT(ltnd),L,I) // < 8
-v(eql,tT(eqlb),s(eqlbB),TT(eqlb),tT(eqlh),s(eqlhH),TT(eqlh),tT(eqli),s(eqliI),TT(eqli),tT(eqll),s(eqllL),TT(eqll),tT(eqld),s(eqldD),TT(eqld),L,I) // = 10
 v0(dex,L,D)v0(add,L,D)v0(sub,L,D)v0(mul,L,D)v0(gtn,L,I)
+//    Bb       Hh       Ii       Ll       Dd
+v(dvd,Tt(dvdb),Tt(dvdh),Tt(dvdi),Tt(dvdl),Tt(dvdd),L,D) // % 4
+v(mod,Tt(modb),Tt(modh),Tt(modi),Tt(modl),Tt(modd),L,D) // ! 5
+v(mnm,s(mnmbB),s(mnmhH),s(mnmiI),s(mnmlL),s(mnmdD),L,D) // & 6
+v(mxm,s(mxmbB),s(mxmhH),s(mxmiI),s(mxmlL),s(mxmdD),L,D) // | 7
+v(ltn,Tt(ltnb),Tt(ltnh),Tt(ltni),Tt(ltnl),Tt(ltnd),L,I) // < 8
+v(eql,s(eqlbB),s(eqlhH),s(eqliI),s(eqllL),s(eqldD),L,I) // = 10
 #undef Tt
-#undef tT
 
 #define h(v,t,T) {v##t##t,v##t##T,v##T##t,v##T##T},
 #define g(a...) {0},{0},{0},{0},h(dvd,a)h(mod,a)h(mnm,a)h(mxm,a)h(ltn,a){0},h(eql,a)
@@ -41,7 +37,7 @@ OV*arf[][11][4]={{g(b,B)},{g(h,H)},{g(i,I)},{g(l,L)},{g(d,D)}};
 #undef g
 #undef h
 
-#define h(v,T,R,r0) SN I v##T##r(R*RE a,T*RE b,UL n)_(R c=*a;i(n,c=v##r0(c,b[i]))*a=c;n)
+#define h(v,T,R,Q) SN I v##T##r(R*RE a,T*RE b,UL n)_(R c=*a;i(n,c=v##Q(c,b[i]))*a=c;n)
 #define g(a...) h(dex,a)h(add,a)h(sub,a)h(mul,a)h(dvd,a)h(mod,a)h(mnm,a)h(mxm,a)h(ltn,a)h(gtn,a)h(eql,a)
 g(B,L,l)g(H,L,l)g(I,L,l)g(L,L,l)g(D,D,d)
 #undef g
