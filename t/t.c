5e9d9bef1c7f472bd75de7268002ce01f14eae12
ngn <@>
2021-03-15 07:19:43 +0200

    t/t.c

--- t/t.c
+++ t/t.c
@@ -20,8 +20,9 @@
 #define wr write
 #define cl close
 typedef char C;typedef int I;typedef long long L;
-S C*mm(C*f,C**e)_(I d=open(f,0);struct stat h;fstat(d,&h);L n=h.st_size;
- C*s=mmap(0,n,PROT_READ,MAP_PRIVATE,d,0);cl(d);*e=s+n;s)
+S L szf(I f)_(struct stat h;fstat(f,&h);h.st_size)
+S L sz(C*s)_(I f=open(s,0);L n=szf(f);cl(f);n)
+S C*mm(C*s,C**e)_(I f=open(s,0);L n=szf(f);C*r=mmap(0,n,PROT_READ,MAP_PRIVATE,f,0);cl(f);*e=r+n;r)
 S I nl(C*s,I n)_(C*p=s;I i=0;W(i<n,Y(s[i]==10&&i<n-1&&s[i+1]==32,*p++=';';i+=2)E(*p++=s[i++]))p-s)
 S I t(C*s,I n)_(write(1,".",1);P(*s=='/'||*s==10,0)
  C*u=strstr(s," /");P(!u,wr(1,"bad test: ",10);wr(1,s,n);-1)
@@ -31,5 +32,5 @@ S I t(C*s,I n)_(write(1,".",1);P(*s=='/'||*s==10,0)
  C o[256];L m=0;W(1,L k=read(p[2],o+m,Z(o)-1-m);Y(k<=0,BR)m+=k;Y(m<Z(o)-1,BR))
  cl(p[2]);m=nl(o,m);u+=3;kill(c,SIGKILL);P(s+n==u+m&&!strncmp(o,u,m),1)
  wr(1,"\nfail: ",6);wr(1,s,n);wr(1,o,m);wr(1,"\n",1);-1)
-I main()_(C*e,*s=mm("t.k",&e);I n=0,f=0;W(s<e,C*u=strchr(s,10)+1;I r=t(s,u-s);n+=!!r;f+=r<0;s=u)
- P(f,pr("\nfail %d/%d\n",f,n);1)pr("\n");0)
+I main()_(pr("size:%lld\n",sz("../k"));C*e,*s=mm("t.k",&e);I n=0,f=0;
+ W(s<e,C*u=strchr(s,10)+1;I r=t(s,u-s);n+=!!r;f+=r<0;s=u)P(f,pr("\nfail %d/%d\n",f,n);1)pr("\n");0)
