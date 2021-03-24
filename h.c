84c6ce6b727fe5908fa37ac27e2d0ca110b21ff8
ngn <@>
2021-03-24 06:38:11 +0200

    h.c

--- h.c
+++ h.c
@@ -5,8 +5,8 @@ A rsh_(Ax/*0*/,L m,L*pj,L*s,L r)_(Nn=absL(*s);P(r>1,Ay=aA(n);i(n,ya=rsh_(x,m,pj,
 SN A flt(Ax,Ay,Cb)_(Er(!ytT,x,y)Au=atn(yt,0);
  i(yn,Az=get(y,i);z=app(x,&z,1);Y(!z,u=u(0);BR)z=gL(z);P(!z,x(y(u(0))))Ln=gl(z);j(b?n:!n,uq(get(y,i))))x(y(u)))
 A2(rsh,XF(flt(x,y,1))Ym(Xz(Az=kv(&y);xR;y=N2(x,z,rsh(x,y));z=N1(y,rsh(x,z));am(y,z))x=enla(x);xR;am(x,ap1(y,x)))
- YM(y=mut(y);yy=ear(cv('#'),A(x,yy),2);y)
- y=!ytT?enl(y):!yn?enl(fir(y)):y;x=enla(Ny(gL(x)));P(!xn,fir(x(y)))
+ YM(y=mut(y);yy=ear(cv('#'),A(x,yy),2);y)Yt(rsh(x,enl(y)))P(!yn,rsh(x,enl(fir(y))))
+ x=enla(Ny(gL(x)));P(!xn,fir(x(y)))
  i(xn,Lv=xl;Y(v<0,Ed(xn>2||xn==2&&v-NL,x,y)P(xn==1&&v==NL,x(y))
   P(xn==2,Ed(*xL<=0&&xL[1]<=0||!yn,x,y)*xL==NL?K("{x@:1;(x*!1+(-x)!-1+#y)_y}",x,y):K("{x@:0;((-x)!(#y)*!x)_y}",x,y))))
  Lj=0;x(y(rsh_(y,yn,&j,xL,xn))))
@@ -19,14 +19,13 @@ A1(enl,P(xtl||xtd,AT(tT(xt),mut(x)))P(xtc||xtz||xts,atnv(tT(xt),1,&x))Xm(Ay=kv(&
 A1(enla,Xmt(enl(x))x)
 A2(cat,
  P(xtmM&&ytmM,P(xtm&&ytm,Az=kv(&y);amd(A(x,y,av0,z),4))Ed(!mtc_(xx,yx),x,y)
-              Au=eac(cv(','),A(mR(xy),mR(yy)),2);Y(u,u=aM(mR(xx),u))x(y(u)))
+              Au=eac(cv(','),A(mR(xy),mR(yy)),2);x(y(u?aM(mR(xx),u):0)))
  x=enla(x);P(!xn,x(enla(y)))P(ytT&&!yn,y(x))Y(xtZ&&ytzZ,N(sup(&x,&y)))
  y=enla(y);Y(xt-yt,x=blw(x);y=blw(y))L w=ZT[xt],m=xn,n=yn;Au;
  Y(xr==1&&(m+n)*w<=cap(x),u=AN(m+n,x))E(u=atn(xt,m+n);mc(uC,xC,m*w);Y(xtA,Y(xr==1,AN(0,x))E(mRa(x)))x(0))
  mc(uC+m*w,yC,n*w);Y(ytA,mRa(y))y(u))
 A apv(Ax,OV*v)_(Q(xtT);Nn=xn,w=ZT[xt];
- Y(xr==1&&w+n*w<=cap(x),x=AN(n+1,x))
- E(Ay=atn(xt,n+1);Y(xr>1&&xtA,mRa(x))EY(xtR,x=AT(tL,mut(x)))m2(x,mc(yC,xC,n*w));x=y)
+ Y(xr==1&&(n+1)*w<=cap(x),x=AN(n+1,x))E(Ay=atn(xt,n+1);Y(xr>1&&xtA,mRa(x))EY(xtR,x=AT(tL,mut(x)))m2(x,mc(yC,xC,n*w));x=y)
  V*p=xC+n*w;mc(p,v,w);x)
 A2(apd,Q(xtT||xtM);XM(P(!ytm||!mtc_(xx,yx),apd(Ny(blw(x)),y))x=mut(x);Az=xy=mut(xy);i(zn,_q(za,get(yy,i)))y(x))
  Y(xtZ&&ytz,A1*f=ct[max(xt,yt-tl+tL)];x=Ny(f(x));y=Nx(f(y)))
