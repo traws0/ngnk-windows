412ed7e4fd33326968967e4e6b3decea4d552970
n <>
2021-03-27 14:25:14 +0200

    unh() t/t.c

--- t/t.c
+++ t/t.c
@@ -28,7 +28,7 @@ S I t(C*s,I n)_(write(1,".",1);P(*s=='/'||*s==10,0)
  C*u=strstr(s," /");P(!u,wr(1,"bad test: ",10);wr(1,s,n);-1)
  C*a[]={"../k",0};I p[4];pipe(p);pipe(p+2);
  I c=fork();P(!c,dup2(*p,0);dup2(p[3],1);i(4,cl(p[i]))exit(execve(*a,a,0));0)
- cl(*p);cl(p[3]);Y(*s-32,wr(p[1],"\\w ",3))wr(p[1],s,u-s);wr(p[1],"\n",1);cl(p[1]);
+ cl(*p);cl(p[3]);wr(p[1],s,u-s);wr(p[1],"\n\\m\n",4);cl(p[1]);
  C o[256];L m=0;W(1,L k=read(p[2],o+m,Z(o)-1-m);Y(k<=0,BR)m+=k;Y(m<Z(o)-1,BR))
  cl(p[2]);m=nl(o,m);u+=3;kill(c,SIGKILL);P(s+n==u+m&&!strncmp(o,u,m),1)
  wr(1,"\nfail: ",6);wr(1,s,n);wr(1,o,m);wr(1,"\n",1);-1)
