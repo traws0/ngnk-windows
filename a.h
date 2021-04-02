89342703876382479eb600bc3a8c02b6e594097b
n <>
2021-04-02 12:03:48 +0300

    wasm outdexing

--- a.h
+++ a.h
@@ -54,7 +54,7 @@ TD long long L;TD double D;TD unsigned long long UL,A,A0(),A1(A),A2(A,A),A3(A,A,
 #define min(a...) extr(<,a)
 #define max(a...) extr(>,a)
 #define rot(x,y) ({TY(x) x_=(x);TY(y) y_=(y);y?x_<<y_|x_>>Z(x)*8-y_:x;})
-S L absL(Lv)_(v<0?-v:v)S C hx1(Ii)_(i+(i>9?'a'-10:'0'))S I in(Ni,Nn)_(i<n)S I c3(Cc,Cv,Cd)_(c<=v&&v<=d)
+S L absL(Lv)_(v<0?-v:v)S C hx1(Ii)_(i+(i>9?'a'-10:'0'))S I in(Li,Ln)_(0<=i&&i<n)S I c3(Cc,Cv,Cd)_(c<=v&&v<=d)
 S I c09(Cc)_(c3('0',c,'9'))S I caf(Cc)_(c3('a',c,'f'))S I cAz(Cc)_(c3('a',c|32,'z'))S I cA9(Cc)_(cAz(c)||c09(c))
 S I cAZ(Cc)_(c3('A',c,'Z'))S I cAF(Cc)_(c3('A',c,'F'))S I cAf(Cc)_(c3('a',c|32,'f'))
 
@@ -113,7 +113,7 @@ A3 a3,try;
 AA amd,am1,dmd,dm1;
 AX eac,rdc,scn,eap,ear,eal,app,prj,run;
 A aCl(Q),aCm(Q,Q),aCn(Q,N),aCz(Q),apc(A,C),apv(A,OV*),atn(C,N),atnv(C,N,OV*),az(L),cts(A,Q,N),cmdl(Q),cmdm(Q),
- ea1(A1,A),e0(Q),e1(Q,A),e2(Q,A,A),eN(Q,I,OA*),evs(Q,Q),get(A,N),getr(A,N),kv(A*),mf(I,N),room(A,N),wdn(A,N,N,N);
+ ea1(A1,A),e0(Q),e1(Q,A),e2(Q,A,A),eN(Q,I,OA*),evs(Q,Q),get(A,L),getr(A,L),kv(A*),mf(I,N),room(A,N),wdn(A,N,N,N);
 V init(I,Q*),mrn(I,OA*),repl();
 C*sl(C*,L),sup(A*,A*),*syp(L*),tZ(L);
 I asg(Ax),si(Q,I),eS(A,I),mtc_(A,A),syP(Q);
