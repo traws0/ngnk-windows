c512a4377e707f13730f6838312799263f00d60a
n <@>
2021-08-08 14:38:04 +0300

    2.c

--- 2.c
+++ 2.c
@@ -90,7 +90,7 @@ C sup(A*p,A*q)_(Ax=*p,y=*q;Ct=max(tZx(x),tZx(y));*p=x=Ny(ct[t](x));*q=y=Nx(ct[t]
 S A ari(Ax,Ay,Iv)_(P(xtmMA||ytmMA,eac2(x,y,av(v)))N(sup(&x,&y));Ik=xtT<<1|ytT;El(k==3&&xn-yn,x,y)
  Ct=k?min(xt,yt):max(xt,yt);V*a=xtP?(V*)&x:xC,*b=ytP?(V*)&y:yC;V(f,arf[k][tT(t)-tB][v-4]);
  Y(t>tD,t+=tD-td)Y(v>7&&t==tD,t=tI)Y(!k,t+=tc-tC;P(_tP(t),Iv=0;f(a,b,&v,1);x(y(az(v)))))
- Az=xt==t&&xr==1?x:yt==t&&yr==1?y:atn(t,k-1?xn:yn);f(a,b,zC,zn);x==z?y(z):y==z?x(z):x(y(z)))
+ Az=xt==t&&xr==1?x:yt==t&&yr==1?y:atn(t,k-1?xn:yn);f(a,b,zC,zn);x-z?x(y-z?y(z):z):y(z))
 S A ext(Ax,Nn)_(xtm?enl(x):xtt?rsh(az(n),x):x)
 S A2(dct,x=ext(x,yN);y=ext(y,xN);El(xN-yN,x,y)am(x,y))
 
