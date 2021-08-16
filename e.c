1b1a748951747f4d35ba4268d7d5b888f2f99e70
n <@>
2021-08-16 21:38:33 +0300

    e.c

--- e.c
+++ e.c
@@ -6,12 +6,11 @@ NI A e2(Qs,Ax,Ay)_(y(e1(s,x)))
 NI A eN(Qs,In,OA*a)_(mrn(n,a);e0(s))
 SN I eC()_(e.n=0;I(e.s,mr(e.s);e.s=0)e.o=0)
 NI I eS(Ax/*1*/,Ii)_(I(!e.s,e.s=x;e.o=i)E(x(0))0)
-A3(try,x=apn(x,y);P(x,z(x))
- I(ztF,Ik=zK;z=k==1?ap1(z,aCn(e.m,e.n)):k==2?apn(z,a2(aCn(e.m,e.n),a2(e.s?_R(e.s):ac(0),az(e.o)))):z)eC();z)
-A1(epr,I h=64;C s0[3*h+6+SZ e.m],*s=s0;*s++='\'';Mc(s,e.m,e.n);s+=e.n;*s++=10;
+A3(try,x=apn(x,y);P(x,z(x))I(ztF&&zK<3,Ay=aCn(e.m,e.n);z=zK<2?ap1(z,y):apn(z,a2(y,a2(e.s?_R(e.s):oC,az(e.o)))))eC();z)
+A1(epr,I h=64;Cb[3*h+6+SZ e.m],*s=b;*s++='\'';Mc(s,e.m,e.n);s+=e.n;*s++=10;
  I(e.s,Ax=e.s;C*t=xC+e.o,*p=t,*q=t+1;W(p>xC&&p>t-h&&p[-1]&&p[-1]-10,p--)W(q<xC+xn&&q<=t+h&&*q&&*q-10,q++)
   Mc(s,p,q-p);I(p<=t-h,*s=s[1]='.')I(q>t+h,s[q-p-2]=s[q-p-1]='.')s+=q-p;*s++=10;Ms(s,32,t-p);s+=t-p;*s++='^';*s++=10)
- eC();write(1,s0,s-s0);x)
+ eC();write(1,b,s-b);x)
 A die(Qs)_(write(1,s,Sn(s));exit(1);0)
 
 #define g(t,m,n,a...) NI A##n(e##t##n,e##n(#m,##a))
