a7bd6cd3b08354d188c94cffb80f0c15d5e7ae13
n <@>
2021-08-21 14:23:53 +0300

    "",!0 -> ""

--- h.c
+++ h.c
@@ -25,7 +25,7 @@ A1(enla,Xmt(enl(x))x)
 A2(cat,
  P(xtmM&&ytmM,P(xtm&&ytm,Az=kv(&y);amd(A(x,y,av0,z),4))Ed(!mtc_(xx,yx),x,y)
               Az=eac2(_R(xy),_R(yy),CAT);x(y(z?aM(_R(xx),z):0)))
- x=enla(x);P(!xn,x(enla(y)))P(ytT&&!yn,y(x))y=enla(y);I(xtZ&&ytzZ,N(sup(&x,&y)))I(xt-yt,x=blw(x);y=blw(y))
+ x=enla(x);P(ytT&&!yn,y(x))P(!xn,x(enla(y)))y=enla(y);I(xtZ&&ytzZ,N(sup(&x,&y)))I(xt-yt,x=blw(x);y=blw(y))
  Nm=xn,n=yn;x=room(m+n,x);Mc(xC+m*ZT[xt],yC,n*ZT[xt]);y(!ytA?0:yr-1?mRa(y):AN(0,y));x)
 A2(apd,XM(P(!ytm||!mtc_(xx,yx),apd(Ny(blw(x)),y))x=mut(x);Az=xy=mut(xy);i(zn,_q(za,get(yy,i)))y(x))
  Q(xtT);P(xtZ&&ytz,N(sup(&x,&y));ytP?apv(x,&y):y(apv(x,yC)))
