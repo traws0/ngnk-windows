b7d8fb48f3f8093ae99255398bf459c65c0e5240
n <@>
2021-08-27 22:01:38 +0300

    verbs

--- a.h
+++ a.h
@@ -112,9 +112,15 @@ S  C _w(Ax)_(xC[-14])               S A AW( C w,Ax)_(Q(w<6);xC[-14]=w;x)        
 S  C _k(Ax)_(xC[-13])               S A AK( C k,Ax)_(Q(k<9);xC[-13]=k;x)                                 //arity(for funcs)
 S UH _o(Ax)_(xts?x>>32:xtP?0:xH[-7])S A AO(UH o,Ax)_(Xs(x&~(0xffffll<<32)|(UL)o<<32)xH[-7]=o;x)          //offset
 S  N _n(Ax)_(xL[-1])                S A AN(  Nn,Ax)_(Q(n<1ll<<48||n==-1);xL[-1]=n;x)                     //length
-S OA au0=(UL)tu<<56,av0=(UL)tv<<56,aw0=(UL)tw<<56,FIR=au0+3,TIL=au0+5,REV=au0+7,LEN=au0+14,LAS=au0+24,OUT=au0+25,
- ADD=av0+1,MUL=av0+3,MOD=av0+5,MNM=av0+6,MXM=av0+7,LTN=av0+8,EQL=av0+10,CAT=av0+12,EXC=av0+13,RSH=av0+14,CUT=av0+15,
- CST=av0+16,QUE=av0+17,AP1=av0+18,APN=av0+19,COM=av0+24,MKL=av0+25,PLH=av0+26,PRG=PLH;
+
+#define VS U(vc,{':','+','-','*','%','!','&','|','<','>','=','~',',','^','#','_','$','?','@','.','0','1','2','3','4','5'})\
+           U(v1,{sam,flp,neg,fir,sqr,til,whr,rev,asc,dsc,grp,not,enl,nul,len,flr,str,unq,typ,val,u0c,u1c,sam,sam,las,out})\
+           U(v2,{dex,add,sub,mul,dvd,mod,mnm,mxm,ltn,gtn,eql,mtc,cat,exc,rsh,cut,cst,que,ap1,apn,v0c,v1c,dex,com,dex,dex})\
+           U(vn,{___,___,___,___,___,___,___,___,___,___,___,___,___,___,___,___,___,ins,amd,dmd,___,___,___,___,___,___})
+enum{ au0=(UL)tu<<56,FLP,NEG,FIR,SQR,TIL,WHR,REV,ASC,DSC,GRP,NOT,ENL,NUL,LEN,FLR,STR,UNQ,TYP,VAL,U0C,U1C,_U2,_U3,LAS,OUT,
+      av0=(UL)tv<<56,ADD,SUB,MUL,DVD,MOD,MNM,MXM,LTN,GTN,EQL,MTC,CAT,EXC,RSH,CUT,CST,QUE,AP1,APN,V0C,V1C,_V2,COM,MKL,PLH,
+      aw0=(UL)tw<<56,PRG=PLH};
+
 #define ND (D)(0./0.)
 #define WD __builtin_inf()
 #define NL (1ll<<63)
