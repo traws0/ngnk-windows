551d7a28b8daa9ef62ba07bdd213ff9012ffa0d3
n <@>
2021-09-10 04:27:22 +0300

    \\

--- m.c
+++ m.c
@@ -37,12 +37,12 @@ S B s0[1<<16],*s1=s0+1;C*syp(L*p)_(*p<0?s0-*p:(V*)p)
 AQ(syP,In=Sn(s)+1;P(n<6&&(n<5||!(s[3]&128)),Lv=0;Mc(&v,s,n);as(v))Qp=s0+1;W(p<s1,P(!SQ(p,s),as(s0-p))p+=Sn(p)+1)
  P(s1+n>s0+SZ s0,die("syms oom\n"))Mc(s1,s,n);s1+=n;as(s0-s1+n))
 
-S Q skp(Qs)_(W(!MQ(s,"/\n",2),Qp=SS(s+1,"\n\\\n");s=p?p+3:s+Sn(s))s)S I ln(Q,Q);S AQ(cmdx,en0())
+S Q skp(Qs)_(W(!MQ(s,"/\n",2),Qp=SS(s+1,"\n\\\n");s=p?p+3:s+Sn(s))s)S I ln(Q,Q);S AQ(cmd_,en0())S AQ(cmdX,exit(0);0)
 S AQ(cmdt,Ln=*s==':'?++s,pl(&s):1,t=now();Ax=N(cpl(N(prs(aCl(s)))));i(n,mr(Nx(app(x,0,0))))x(az((now()-t+500)/1000)))
 AQ(cmdl,Ax=N(u1c(aCl(s)));Ed(!xn||xC[xn-1]-10,x)xC[xn-1]=0;Qp=xV;I(!MQ(p,"#!",2),p=SC0(p+2,10))
  W(*p,Qq=p=skp(p);W(*q&&(*q-10||si(" }",q[1])<2),q++)Nx(ln(p,q));p=q+!!*q)x(au))
-A evs(Qs,Qq)_(P(*s-'\\',val(aCm(s,q)))Cc=s[1],d=s[2];P(c=='\\',exit(0);0)P(c=='c'&&d=='d'&&SC("\n :",s[3]),cmdcd(s+3))
- P(cA9(c)&&SC("\n :",d),CH(si("ltm",c),&cmdl,cmdt,cmdm,cmdx)(s+2+(d==32)))K("0x0a\\`x(,,\"/bin/sh\"),,:",aCl(s+1)))
+A evs(Qs,Qq)_(P(*s-'\\',val(aCm(s,q)))Cc=s[1],d=s[2];P(c=='c'&&d=='d'&&SC("\n :",s[3]),cmdcd(s+3))
+ P(SC("\n :",d),CH(si("ltm\\",c),&cmdl,cmdt,cmdm,cmdX,cmd_)(s+2+(d==32)))K("0x0a\\`x(,,\"/bin/sh\"),,:",aCl(s+1)))
 S I ln(Qp,Qq)_(Ax=evs(p,q);x=x?out(x):0;x?x(1):epr(0))
 
 A glb,cns,ce[tn],cn[tn],ci[2][5];Q*argv,*env;
