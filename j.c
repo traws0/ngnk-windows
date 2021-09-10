3df2ecd870c0a6a4ed66981ff11f38d09386e948
n <@>
2021-09-10 04:22:27 +0300

    js1()

--- j.c
+++ j.c
@@ -14,6 +14,6 @@ S A jr(Qp,Lv)_(Cc;W((c=*p++),P(c-*s,0)s++)az(v))S A0(jt,jr("true",1))S A0(jf,jr(
 S A0(jv,Cc=jw();Ii=si("-ftn[{\"",c)*!c09(c);Ax=CH(i,&jd,jf,jt,jN,ja,jo,js,je)();jw();x)
 A1(js0,Et(!xtcC,x)x=str0(enla(x));s=xV;Ay=jv();I(y&&*s,y=y(0))P(!y,eS(x,s-(Q)xV);ep0())x(y))
 
-A1(js1,Xz(Lv=gl(x);Qs=!v?"false":v==1?"true":v==NL?"null":0;s?aCz(s):js1(cD(x)))
+A1(js1,Xz(Lv=gl(x);Qs=!v?"false":v==1?"true":v==NL?"null":0;s?aCz(s):js1(ad(v)))
  Xd(str(x))Xc(js1(enl(x)))XC(xn-1?kst(x):kst(fir(x)))
  XMT(sur('[',jc(',',N(eac1(x,js1))),']'))Xm(Ay=kv(&x);sur('{',jc(',',N(K("{(`j'x),'\":\",'`j'y}",x,y))),'}'))js1(str(x)))
