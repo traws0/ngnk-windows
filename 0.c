a9c500601b3c4c09a0ee4f63f7c7f3f5d6da100e
n <@>
2021-09-11 03:42:12 +0300

    option to compile with libc

--- 0.c
+++ 0.c
@@ -29,7 +29,8 @@ I strcmp(Qp,Qq)_(W(*p&&*p==*q,p++;q++)*p-*q)
 #ifndef shared
  I main(In,O char**a)_(kinit(n,a);I r=n>1?!cmdl(a[1]):repl();Q(cmdm(""));exit(r);r)
 #endif
-NWASM(
+
+#if defined(nostdlib)&&!defined(wasm)
  FBSD(V _start(O char**a){main(*(I*)(V*)a,a+1);})
  NFBSD(asm(".globl _start;_start:"I386("pop %eax;push %esp;push %eax;call main")
                                  NI386("pop %rdi;mov %rsp,%rsi;jmp main"));)
@@ -47,7 +48,8 @@ NWASM(
  NFBSD(asm(h(pipe));)FBSD(asm(h(pipe2));I pipe(Iv[2])_(pipe2(v,0)))
  NFBSD(asm(h(getcwd));)FBSD(asm(h(__getcwd));char*__getcwd(char*,N);char*getcwd(char*s,Nn)_(__getcwd(s,n)))
  asm(h3(read)h3(write)h3(open)h1(close)h2(fstat)h3(lseek)h2(munmap)h2(dup2)h3(socket)h5(setsockopt)h3(connect)
-     h(fork)h3(execve)h1(exit)h2(gettimeofday)h6(mmap)h3(getdents)h1(chdir)h2(ftruncate));)
+     h(fork)h3(execve)h1(exit)h2(gettimeofday)h6(mmap)h3(getdents)h1(chdir)h2(ftruncate));
+#endif
 
 #ifdef wasm
  I js_in(V*,N);V js_out(OV*,N);V js_log(OV*);V*js_alloc(N);V js_time(I*,long*);I js_exit(I);
