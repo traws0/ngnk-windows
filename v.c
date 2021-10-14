3b0ebc05f9f421c68e02bf16f6dbac259edb7bb1
n <@>
2021-10-15 00:47:00 +0300

    whr()

--- v.c
+++ v.c
@@ -1,6 +1,4 @@
 #include"a.h" // ngn/k, (c) 2019-2021 ngn, GNU AGPLv3 - https://codeberg.org/ngn/k/blob/master/LICENSE
-#define h(T) SN V w##T(L*RE p,Nn,V*RE r)_(T*a=r;LN(p)LN(a)i(n,j(p[i],*a++=i)))
-h(B)h(H)h(I)h(L)
 #define c_(T,R,p) SN V c##T##R(OV*RE v,V*RE w,Nn)_(O T*RE a=v;R*RE r=w;i(PD(n,p),r[i]=a[i]))
 #define cN(T,R,p) SN V c##T##R(OV*RE v,V*RE w,Nn)_(O T*RE a=v;R*RE r=w;i(PD(n,p),r[i]=a[i]==N##T?N##R:a[i]))
 c_(C,H,r)c_(B,H,r)c_(H,B,a)c_(C,I,r)c_(B,I,r)c_(I,C,a)c_(I,B,a)c_(L,B,a)
@@ -17,7 +15,10 @@ AL(tilL,P(n<0,n-NL?add(az(n),N(tilL(-n))):ed0())Ez(n>1<<30)Ax=atn(tZ(n-1),n);til
 A1(til,Xz(tilL(gl(x)))XZ(K("{x((*a)#&#)'1_a:|*\\|x,1}",x))XmM(x(_R(xx)))Xo(val(x))Et(!xtA||xn,x)x)
 A1(whr,Xt(XF(et1(x))whr(enl(x)))XA(K("{$[`A~@x;(,&#'*'x),,'/x@\\:!0|/#'x:o'x;,&x]}",x))
  Xm(Ay=kv(&x);idx(x,Nx(whr(y))))XM(et1(x))
- x=N(cL(x));Ln=0;i(xn,n+=xl;Ed(n<0||xl<0,x))Ct=tZ(xn-!!xn);Ez(t>tI,x)Ay=atn(t,n);x2(n=0;CH(t-tB,&wB,wH,wI)(xV,xn,yV);y))
+ x=N(cL(x));Ln=0;i(xn,n+=xl;Ed(n<0||xl<0,x))Ct=tZ(xn-!!xn);P(t>tI,ez1(x))
+ Ay=atn(t,n);x2(I(t==tB,B*a=yV;i(xn,j(xl,*a++=i)))
+               EI(t==tH,H*a=yV;i(xn,j(xl,*a++=i)))
+               E(       I*a=yV;i(xn,j(xl,*a++=i)))y))
 A1(rev,Xm(Ay=kv(&x);am(rev(x),rev(y)))XM(Ay=kv(&x);aM(x,Nx(eac1(y,rev))))Xt(x)x=mut(x);Ii=0,j=xn-1,w=Tz[xt];
  W(i<j,I(!w,SWP(xb,xB[j]))EI(w==1,SWP(xh,xH[j]))EI(w==2,SWP(xi,xI[j]))E(SWP(xl,xL[j]))i++;j--)x)
 A1(typ,x(as(TS[xt])))
