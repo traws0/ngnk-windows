b60faa590a9c1ccad79d78a5d747fcf62f5e58ce
n <@>
2021-10-25 07:32:57 +0300

    b.c

--- b.c
+++ b.c
@@ -4,10 +4,10 @@
 enum{bu,bv=32,bs=64,bg=80,bd=96,bm=112,bM,bl,bL,ba,bP,bz,bj,bo,bp,b4,bc=128,bC=255};
 #define h(a) {m[nb]=o;b[nb]=a;nb+=nb<SZ b-1;}       //add instruction
 #define hc(a) {Ij=bc+fpA(&fc,a);P(j>bC,ez0())h(j);} //add a "load constant" instruction
-#define OK 1
+#define OK -1
 #define Nr(a...) {I r=cr(a);P(r-OK,r);}
 #define Nl(a...) {I r=cl(a);P(r-OK,r);}
-S UC b[2048],m[SZ b];S A fl,fc;S I nb,ns,ms,fu[16];S A1 cr;
+S UC b[2048],m[SZ b];S A fl,fc;S I nb,fu[16];S A1 cr;
 S A2(cl,/*0*/I o=xo;Q(xx==av||_t(xx)==tu);Iv=_v(xx);
  Ys(P(xx==av&&_n(fl),Li=fpI(&fl,yv);P(i>15,ez0())fu[i]=nb;h(bs|i);OK)
   hc(oA);Ik=yv;Li=fAI(fl,k);I(i<0,hc(as(k));h(bM))E(fu[i]=nb;h(bm);h(i))h(v);OK)
@@ -39,29 +39,29 @@ A1(cf,XA(P(xx==MKL,i(xn,Ay=xa;YsSA(x))a1(drp(1,x)))
          P(xn==2?f2(xx,xy):xn==3?f3(xx,xy,xz):0,qt(N(val(x))))
          AO(xo,blw(eac1(x,cf))))x)
 A3(cpl,/*src,ast,loc*/y=cf(y);P(!y,x(z(0)))
- nb=ns=ms=0;fl=z;fc=a1(au);Ms(fu,-1,SZ fu);I r=cr(y);y(0);I o=0;h(bu);P(r-OK,eS(xR,r);ec0())
+ nb=0;fl=z;fc=a1(au);Ms(fu,-1,SZ fu);I r=cr(y);y(0);I o=0;h(bu);P(r-OK,eS(xR,r);ec0())
  P(nb>=SZ b-1,eS(xR,r);ez0())i(16,Ij=fu[i];I(j>=0&&b[j]==bg,b[j]=bd))AK(0,aV(to,5,A(x,aCn(b,nb),aCn(m,nb),fl,fc))))
 
 S A s0[65536],*s=s0+ZZ(s0);
 S V mrn0(A*a,Nn){i(n,Ax=a[i];I(x,x(0)))}
 AX(run,Q(xto);Q(n==xk);S I d;P(++d>1024,esn(a,n))
- UC*p=_V(xy);Im=_n(xA[3]);A*t=s,l[m],r;Mc(l,a,n*SZ x);i(m-n,l[n+i]=au)
- W(1,UC b=*p++;
-  I(b>=bc,*--s=_R(_A(xA[4])[b-bc]))
-  EI(b>=bm,
-   I(b==bm,A*v=l+*p++,x=*v,y=*s++,z=*s;*v=*s=au;*v=dmn(A(x,yR,av+*p++,z),4);B(!*v,r=y(0))*s=apn(_R(*v),y);B(!*s,r=0))
-   EI(b==bM,Ax=*s++,y=*s++,z=*s;*s=au;A w=mnd(A(x,yR,av+*p++,z),4,dmn);B(!w,r=y(0))*s=apn(w,y);B(!*s,r=0))
-   EI(b==bl,Nn=*p++;s+=n;Ax=sqz(aV(tA,n,s-n));*--s=x)
-   EI(b==bL,Nn=*p++;Ax=*s;B(!xtt&&xN-n,r=el0())i(n,*--s=get(x,n-1-i)))
-   EI(b==ba||b==bP,Nn=*p++;Ax=s[n]=(b-ba?prj:app)(*s,s+1,n);mr(*s);s+=n;B(!x,r=0))
-   EI(b==bz,p+=1+*p*!tru(*s++))
-   EI(b==bj,p+=*p)
-   EI(b==bo,*--s=xR)
-   EI(b==bp,mr(*s++))
-   EI(b==b4,s+=3;*s=dmd(aV(tA,4,s-3));B(!x,r=0)))
-  EI(b>=bs,A*v=l+b%16,x=*v;I(b>=bd,*--s=x;*v=au)EI(b>=bg,*--s=xR)E(x=x(*v=_R(*s))))
-  EI(b>=bv,Ax=*s++;*s=v2[b-bv](x,*s);B(!*s,r=0))
-  EI(b,*s=v1[b](*s);B(!*s,r=0))
+ UC*b=_V(xy);Im=_n(xA[3]);A*t=s,l[m],r;Mc(l,a,n*SZ x);i(m-n,l[n+i]=au)
+ W(1,UC c=*b++;
+  I(c>=bc,*--s=_R(_A(xA[4])[c-bc]))
+  EI(c>=bm,
+   I(c==bm,A*v=l+*b++,x=*v,y=*s++,z=*s;*v=*s=au;*v=dmn(A(x,yR,av+*b++,z),4);B(!*v,r=y(0))*s=apn(_R(*v),y);B(!*s,r=0))
+   EI(c==bM,Ax=*s++,y=*s++,z=*s;*s=au;A w=mnd(A(x,yR,av+*b++,z),4,dmn);B(!w,r=y(0))*s=apn(w,y);B(!*s,r=0))
+   EI(c==bl,Nn=*b++;s+=n;Ax=sqz(aV(tA,n,s-n));*--s=x)
+   EI(c==bL,Nn=*b++;Ax=*s;B(!xtt&&xN-n,r=el0())i(n,*--s=get(x,n-1-i)))
+   EI(c==ba||c==bP,Nn=*b++;Ax=s[n]=(c-ba?prj:app)(*s,s+1,n);mr(*s);s+=n;B(!x,r=0))
+   EI(c==bz,b+=1+*b*!tru(*s++))
+   EI(c==bj,b+=*b)
+   EI(c==bo,*--s=xR)
+   EI(c==bp,mr(*s++))
+   EI(c==b4,s+=3;*s=dmd(aV(tA,4,s-3));B(!x,r=0)))
+  EI(c>=bs,A*v=l+c%16,x=*v;I(c>=bd,*--s=x;*v=au)EI(c>=bg,*--s=xR)E(x=x(*v=_R(*s))))
+  EI(c>=bv,Ax=*s++;*s=v2[c-bv](x,*s);B(!*s,r=0))
+  EI(c,*s=v1[c](*s);B(!*s,r=0))
   E(B(1,r=*s++)))
- I(!r,eS(_R(xx),_C(xz)[p-1-_C(xy)]);mrn0(s,t-s);s=t)
+ I(!r,eS(_R(xx),_C(xz)[b-1-_C(xy)]);mrn0(s,t-s);s=t)
  mrn0(l,m);d--;r)
