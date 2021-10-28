3fcce783b52820b61bbb2bf71c29dfbea668a5c8
n <@>
2021-10-29 00:57:59 +0300

    w.c

--- w.c
+++ w.c
@@ -12,11 +12,14 @@ A jC(Qs,Nm,Ax)_(Ln=jN(m,x);P(n<0,et1(x))Ay=aC(n);C*p=yV;i(xn,I(i,Mc(p,s,m);p+=m)
 L cfm(OA*a/*0*/,In)_(Lm=-1;i(n,Ax=a[i];I(!xtt,Lv=xN;P(m>=0&&m-v,-2)m=v))m)
 S AX(eacm,Az=oS;Ik=0;i(n,Ay=a[i];I(ytm,k++;z=cat(z,_R(yx))))I(k>1,z=unq(z))
  Ab8;i(n,Ay=b[i]=a[i];I(ytm,Au=kv(&y);uq(id_(x,u));b[i]=ap1(u,fil(ai(yn),fnd(y,zR)))))x=eac(x,b,n);P(!x,z(x))dct(z,x))
-AX(eac,I(n==1,Xu(eac1f(*a,v1[xv])))J(n==2,Xv(eac2f(*a,a[1],v2[xv])))
+A2(eac1,/*01*/Xu(eac1f(y,v1[xv]))Yt(app(x,&y,1))Ym(eacm(x,&y,1))
+ Ct=ytA?2+(yr>1):4;Nm=yN;Au=x==LEN?oB:oA;I(t==2,y=AN(0,y)) //2:tA(r=1),3:tA,4:other
+ i(m,Az=app(x,A(t==2?ya:get(y,i)),1);B(!z,u=u(0);I(t==2,mrn(m-i-1,yA+i+1)))uq(z))y(u))
+AX(eac,P(n==1,eac1(x,*a))J(n==2,Xv(eac2f(*a,a[1],v2[xv])))
  Ab8;Ct[8];Lm=-1;
  i(n,Ay=b[i]=a[i];Ym(eacm(x,a,n))t[i]=ytP?0:ytt?1:ytA?2+(yr>1):4;I(t[i]>1,L l=yN;P(m>=0&&m-l,eln(a,n))m=l))
  P(m<0,app(x,a,n))i(n,I(t[i]==1,_r(a[i])+=m)) //0:pkdatm,1:refatm,2:tA(r=1),3:tA,4:other
- Au=0;I(!m,I(x==LEN,u=oB)J(n==2&&xtv&&xv<11,u=a[!_N(a[1])];uR)E(u=oA))
+ Au=0;I(!m,u=x==LEN?oB:n==2&&xtv&&xv<11?_R(a[!_N(a[1])]):oA)
  j(m,i(n,Ay=a[i];I(t[i]==2,b[i]=yA[j])I(t[i]>2,b[i]=get(y,j)))Az=app(x,b,n);
      B(!z,I(u,u=u(0))i(n,Ay=a[i];I(t[i]==1,yr-=m-j-1)I(t[i]==2,mrn(m-j-1,yA+j+1))))
      I(!j,u=c3(ti,zt,ts)?AN(0,atn(TT[zt],m)):oA)uq(z))
@@ -56,9 +59,9 @@ S A3(eap2,/*011*/P(ztm,Au=kv(&z);am(z,Nz(eap2(x,y,u))))P(!ztT,et2(y,z))P(!zn,y(z
  Au=oA;i(zn,y=app(x,A(get(z,i),y),2);P(!y,z(u(0)))uq(y);y=get(z,i))y(z(u)))
 S A2(eap1,/*01*/Ym(Az=kv(&y);am(y,Ny(eap1(x,z))))eap2(x,id_(x,y),y))
 S A3(ste,/*011*/K("{x'y'z}",xR,y,z))
-A3(ear2,/*011*/x=prj(x,A(y,PLH),2);x(eac(x,&z,1)))
-A3(eal2,/*011*/x=prj(x,A(PLH,z),2);x(eac(x,&y,1)))
-AX(eal,Ab8;Mc(b,a,n*SZ x);*b=PLH;x=prj(x,b,n);x(eac(x,a,1)))
+A3(ear2,/*011*/x=prj(x,A(y,PLH),2);x(eac1(x,z)))
+A3(eal2,/*011*/x=prj(x,A(PLH,z),2);x(eac1(x,y)))
+AX(eal,Ab8;Mc(b,a,n*SZ x);*b=PLH;x=prj(x,b,n);x(eac1(x,*a)))
 AX(w0,I(n==1, Xz(win(xR,*a))XT(bin(xR,*a)))                              eac(x,a,n)) /* '  */
 AX(w1,I(n==1,XzZ(dec(xR,*a))Xc(jc(xv,*a))XC(jC(xV,xn,*a)))xK<2?lr(x,a,n):rdc(x,a,n)) /* /  */
 AX(w2,I(n==1,XzZ(enc(xR,*a))Xc(sc(xv,*a))XC(sC(xV,xn,*a)))xK<2?ls(x,a,n):scn(x,a,n)) /* \  */
