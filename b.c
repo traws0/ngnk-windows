30ce8f660fca186fca6469d222de2c009ccaeb83
n <@>
2021-11-10 02:39:51 +0200

    b.c

--- b.c
+++ b.c
@@ -1,5 +1,5 @@
 #include"a.h" // ngn/k, (c) 2019-2021 ngn, GNU AGPLv3 - https://codeberg.org/ngn/k/raw/branch/master/LICENSE
-enum{bu,bv=32,bs=64,bg=72,bd=80,bi=88,bI,bm,bM,bG,bS,bl,bL,ba,bP,bz,bj,bo,bp,b4,bc};
+enum{bu,bv=32,bs=64,bg=72,bd=80,ba=88,bP=96,bi,bm,bM,bI,bG,bS,bl,bL,bz,bj,bA,bo,bp,bc};
 #define h(a) {b[nb]=a;m[nb]=o;nb+=nb<SZ b-1;}
 #define hc(a) {N(lc(a,o));}
 #define OK -1
@@ -12,9 +12,9 @@ SN I il(Iv)_(Li=fAI(fl,v);P(i>=0,lu[i]=nb;i)-1)
 S A2(cl,/*00*/I o=xo;Q(xx==av||_t(xx)==tu)Iv=_v(xx);
  Ys(P(xx==av&&_n(fl),Li=fpI(&fl,yv);P(i>15,ez0())lu[i]=nb;h(bs|i)OK)
   Ii=il(yv);P(i>=0,h(bm)h(i)h(v)OK)i=ig(yv);P(i<0,ez0())P(!v,h(bS)h(i)OK)h(bM)h(i)h(v)OK)
- YS(hc(av+v)hc(au)hc(yR)h(b4)OK)
+ YS(hc(av+v)hc(au)hc(yR)h(bA)OK)
  YA(In=yn-1;P(n-(UC)n,o)Az=yx;
-  P(ztS,hc(av+v)i(n,Nr(yA[n-i]))h(bl)h(n)hc(zR)h(b4)OK)
+  P(ztS,hc(av+v)i(n,Nr(yA[n-i]))h(bl)h(n)hc(zR)h(bA)OK)
   P(zts,i(n,Nr(yA[n-i]))h(bl)h(n);Ii=il(zv);I(i<0,hc(z)h(bI))E(h(bi)h(i))h(v)OK)
   P(z==MKL&&xx==av,h(bL)h(n);i(n,Nl(x,yA[i+1]);h(bp))OK)o)o)
 S A1(cr,/*0*/I o=xo;
@@ -26,14 +26,15 @@ S A1(cr,/*0*/I o=xo;
  P(n<2,hc(yR)OK)
  P(n==3&&(ytu||y==av)&&_tsSA(xy),Nr(xz);Nl(x,xy);OK)
  P(n>3&&y==CST,n--;I p[n];A*a=xA;i(n&~1,Nr(*++a);h(i&1?bj:bz)p[i]=nb;h(0))Nr(n&1?*++a:au);
-  i(n&~1,I d=(i&1?nb:p[i+1])-p[i];I(i&1,Ij=(n&~1)-1;W(i<j&&d>255,d=p[j]-1-p[i];j-=2))P(d>255,ez0())b[p[i]]=d)OK)
+  i(n&~1,I d=(i&1?nb-1:p[i+1])-p[i];I(i&1,Ij=(n&~1)-1;W(i<j&&d>255,d=p[j]-1-p[i];j-=2))P(d>255,ez0())b[p[i]]=d)OK)
  I(n==2&&y==FIR,Az=xy;P(ztA&&zn==2&&zx==REV,Nr(zy);h(bu|LAS-au)OK))
  I p=0;i(n-1,Az=xA[n-1-i];I(z==PLH,p=1;hc(PLH))E(Nr(z)))
  P(p,Nr(xx);h(bP)h(n-1)OK)
  P(y==MKL,n--;P(n-(UC)n,o);h(bl)h(n)OK)
  P(n==2&&ytu,h(bu|yv)OK)
  P(n==3&&ytv,h(bv|yv)OK)
- Nr(xx);h(ba)h(n-1)OK)
+ P(n<=8,Nr(xx);h(ba+n-2)OK)
+ o)
 S A2(f2,/*0*/P(xtw&&!ytsSA,1)/*P(x==TIL&&ytZ&&yn<4,i(yn,P(!in(gl(ii(y,i)),101),0))1)*/0)
 S A3(f3,/*0*/P(ADD<=x&&x<=MUL&&ytL&&ztL&&yn==zn&&yn<101,1)0)
 S A1(qt,/*1*/xtsSA?enl(x):x)
@@ -42,37 +43,41 @@ S A1(cf,XA(P(xx==MKL,i(xn,Ay=xa;YsSA(x))a1(drp(1,x)))
            AO(xo,cA(eac1f(x,cf))))x)
 S I mxs(Ii,I s)_(I r=s;W(1,UC c=b[i++];Q(s>=0)r=max(r,s);P(!c,r)P(c==bz,i++;s--;max(r,max(mxs(i,s),mxs(i+b[i-1],s))))
  I(c>=bc||c==bo||c-bg<16u,s++)J(c-bv<32u||c==bp,s--)J(c==bm,i+=2)J(c==bi,i+=2;s--)J(c==bM,i+=2)J(c==bI,i++;s-=2)
- J(c==bL,s+=b[i++])J(c==bl||c==ba||c==bP,s-=b[i++]-1)J(c==bj,i+=b[i])J(c==b4,s-=3)J(c==bG,i++;s++)J(c==bS,i++))r)
+ J(c==bL,s+=b[i++])J(c==bl||c==bP,s-=b[i++]-1)J(c==ba,s-=c-ba+1)J(c==bj,i+=1+b[i])J(c==bA,s-=3)J(c==bG,i++;s++)J(c==bS,i++))r)
 A3(cpl,/*src,ast,loc*/y=cf(y);P(!y,x(z(0)))nb=1;Ik=zn;fl=z;fc=a1(au);Ms(lu,-1,SZ lu);I r=cr(y);y(0);I o=0;h(bu);
  P(r-OK,eS(y(z(x)),r);ec0())P(_n(fl)>8||nb>=SZ b-1,eS(y(z(x)),0);ez0())i(8,Ij=lu[i];I(j>=0&&b[j]==bg,b[j]=bd))
  *b=mxs(1,0);*m=-1;AK(k,AT(to,cat(aV(tA,4,A(x,aCn(b,nb),aCn(m,nb),fl)),fc))))
 
-S V mrn0(A*a,Nn){i(n,I(a[i],mr(a[i])))}
 #define p(x) ({A x_=(x);Q(x_)Q(t<s)*--s=x_;})
 #define q() ({Q(s<t+ns)Q(*s)Ax=*s;*s++=0;x;})
+#define U(x,a...) B(!(x),a)
 AX(run,Q(xto)P(n-xk,er8(a,n))S I d;P(++d>1024,es8(a,n))
  UC*b=_V(xy),c,ns=*b++,nl=_n(xA[3]);A t[ns+nl],*s=t+ns,*l=s;Ms(t,0,SZ t);Mc(l,a,8*n);
  W((c=*b++),
   I(c>=bc,p(_R(xA[c-bc+4])))
-  J(c>=bi,
-   I(c==bi,A*v=l+*b++,x=*v?*v:au,y=q(),z=*s;*v=*s=0;*v=dm8(A(x,yR,av+*b++,z),4);B(!*v,mr(y))*s=dot(_R(*v),y);B(!*s))
-   J(c==bI,Ax=q(),y=q(),z=*s;*s=0;A w=mnd(A(x,yR,av+*b++,z),4,dm8);B(!w,mr(y))*s=dot(w,y);B(!*s))
-   J(c==bm,A*v=l +*b++;            *s=*v=v2[*b++](*v,*s);B(!*s)_R(*s))
-   J(c==bM,A*v=gv+*b++;B(!*v,ev0())*s=*v=v2[*b++](*v,*s);B(!*s)_R(*s))
-   J(c==bG,A*v=gv+*b++;B(!*v,ev0())p(_R(*v)))
-   J(c==bS,A*v=gv+*b++;I(*v,mr(*v))*v=_R(*s);)
-   J(c==bl,Nn=*b++;s+=n-1;*s=sqz(aV(tA,n,s-n+1)))
-   J(c==bL,Nn=*b++;Ax=*s;B(!xtt&&xN-n,*s=el1(x))i(n,p(ii(x,n-1-i))))
-   J(c==ba||c==bP,Nn=*b++;s[n]=(c-ba?prj:_8)(*s,s+1,n);mr(*s);s+=n;B(!*s))
-   J(c==bz,b+=1+*b*!tru(q()))
-   J(c==bj,b+=*b)
+  J(c>=bA,
+   I(c==bA,s+=3;U(*s=dmd(s-3,4)))
    J(c==bo,p(xR))
-   J(c==bp,mr(q()))
-   J(c==b4,s+=3;*s=dmd(s-3,4);B(!*s)))
-  J(c>=bd,A*v=l+c-bd;p(*v?*v:au);*v=0)
-  J(c>=bg,A*v=l+c-bg;p(_R(*v?*v:au)))
-  J(c>=bs,A*v=l+c-bs,y=_R(*s);*v=*v?dex(*v,y):y)
-  J(c>=bv,Ax=q();*s=v2[c-bv](x,*s);B(!*s))
-  E(B(!(*s=v1[c](*s)))))
- I(!*s,eS(_R(xx),_C(xz)[b-1-_C(xy)]);mrn0(s+1,t+ns-s-1))
- mrn0(l,nl);d--;Q(s==l-1)*s)
+   E(Q(c==bp)mr(q())))
+  J(c>=bP,Nn=*b++;
+   I(c==bP,Ax=*s;s+=n;U(*s=x(prj(x,s-n+1,n))))
+   J(c==bi,A*v=l+n,y=q(),x=*s=*v=dm8(A(*v?*v:au,yR,av+*b++,*s),4);U(x,mr(y))U(*s=dot(xR,y)))
+   J(c==bm,A*v=l+n;U(*s=*v=v2[*b++](*v,*s))_R(*s))
+   J(c==bI,Ax=q(),y=q();x=mnd(A(x,yR,av+n,*s),4,dm8);U(*s=x?dot(x,y):y(x)))
+   J(c==bM,Ax=gv[n];U(x,*s=ev1(*s))x=*s=gv[n]=v2[*b++](x,*s);U(x)xR)
+   J(c==bG,Ax=gv[n];U(*--s=x,ev0())xR)
+   J(c==bS,Ax=gv[n];I(x,mr(x))gv[n]=_R(*s))
+   J(c==bl,A*p=s;s+=n-1;*s=sqz(aV(tA,n,p)))
+   J(c==bL,Ax=*s;U(xtt||xN==n,*s=el1(x))i(n,p(ii(x,n-1-i))))
+   J(c==bz,b+=n*!tru(q()))
+   E(Q(c==bj)b+=n))
+  J(c>=ba,Nn=c-ba+1;Ax=*s;s+=n;U(*s=x(_8(x,s-n+1,n))))
+  J(c>=bs,A*v=l+c%8,x=*v;
+   I(c>=bd,p(x);*v=0)
+   J(c>=bg,p(x?xR:au))
+   E(Q(c>=bs)Ay=_R(*s);*v=x?x(y):y))
+  J(c>=bv,U(*s=v2[c-bv](q(),*s)))
+  E(U(*s=v1[c](*s))))
+ I(c,eS(_R(xx),_C(xz)[b-1-_C(xy)]))
+ i(t+ns+nl-s-1,Ax=s[i+1];I(x,mr(x)))
+ d--;Q(s==l-1)*s)
--- vim-c/syntax/n.vim
+++ vim-c/syntax/n.vim
@@ -21,7 +21,7 @@ R n_bb g=n_b s="\["                      e="]" c=@n_x,n_b |M n_b "," contained |
 R n_cc g=n_c s="{"                       e="}" c=@n_x,n_c |M n_c "," contained |H n_c n_a
 R n_pp g=n_p s="\v<(P|E[a-z]|[XY]\w+)\(" e=")" c=@n_x,n_p |M n_p "," contained |H n_p function
 R n_ff g=n_f s="\<\([ijW]\|iu\w\)("      e=")" c=@n_x,n_f |M n_f "," contained |H n_f special
-R n_ii g=n_i s="\<\([IJEB]\)("           e=")" c=@n_x,n_i |M n_i "," contained |H n_i type
+R n_ii g=n_i s="\<\([IJEBU]\)("          e=")" c=@n_x,n_i |M n_i "," contained |H n_i type
 R n_vv g=n_v s="(\(\w\| \)*,"            e=")" c=@n_x contained                |H n_v statement
 R n_uu g=n_u s="\<_("                    e=")" c=@n_x                          |H n_u n_v
 sy cluster n_x contains=n_comment,n_h,n_string,n_k,n_t,n_r,n_aa,n_bb,n_cc,n_pp,n_ff,n_ii
