20feef0629ff1081585be78f29d94c656f37b9a1
n <@>
2021-11-24 21:59:24 +0200

    k.c

--- k.c
+++ k.c
@@ -1,10 +1,21 @@
 #include"a.h" // ngn/k, (c) 2019-2021 ngn, GNU AGPLv3 - https://codeberg.org/ngn/k/raw/branch/master/LICENSE
 #include"k.h"
 #define Kx K x
-K Kc(Cv)_(ac(v))K Ks(C*s)_(syP(s))K Ki(Iv)_(ai(v))K Kf(Dv)_(ad(v))
-K KC(C*a,Nn)_(aV(tC,n,a))K KS(C**a,Nn)_(Ax=aS(n);i(n,xi=syP(a[i]))x)K KI(I*a,Nn)_(aV(tI,n,a))K KF(D*a,Nn)_(aV(tD,n,a))
-C TK(Kx)_(Tk[xt])N NK(Kx)_(xn)
-C cK(Kx)_(xv)I iK(Kx)_(xv)D fK(Kx)_(*xD)
-S V VK(V*a,Kx){Mc(a,xV,xn*xW);}V CK(C*a,Kx){VK(a,x);}V IK(I*a,Kx){VK(a,x);}V FK(D*a,Kx){VK(a,x);}
-K ref(Kx)_(xR)K unref(Kx)_(x(x))
-//K K1(C,K),K2(C,K,K),KR(V*,I,C*),tri(K,K,K);V KA(K,K);K Kp(V*);
+#define Ky K y
+C TK(Kx)_(Tk[xt])
+N NK(Kx)_(xn)
+V*dK(Kx)_(xV)
+S V VK(V*a,Kx){Mc(a,xV,xn*xW);}
+
+K Kc(Cv)_(ac(v))     C cK(Kx)_(xv)  K KC(C*a,Nn)_(aV(tC,n,a))                   V CK(C*a,Kx){VK(a,x);}
+K Ks(C*s)_(syP(s))                  K KS(C**a,Nn)_(Ax=aS(n);i(n,xi=syP(a[i]))x)
+K Ki(Iv)_(ai(v))     I iK(Kx)_(xv)  K KI(I*a,Nn)_(aV(tI,n,a))                   V IK(I*a,Kx){VK(a,x);}
+K Kf(Dv)_(ad(v))     D fK(Kx)_(*xD) K KF(D*a,Nn)_(aV(tD,n,a))                   V FK(D*a,Kx){VK(a,x);}
+K Kp(V*p)_(al((L)p))
+
+K K1(Cv,Kx)_(v1[si(vc,v)](x))
+K K2(Cv,Kx,Ky)_(v2[si(vc,v)](x,y))
+V KA(Kx,Ky){} //nyi
+K KR(V*f,In,C*s)_(0) //nyi
+K ref(Kx)_(xR)
+K unref(Kx)_(x(x))
