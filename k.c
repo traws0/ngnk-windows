ccb29dfa2a4797cfe4e4576315ae0e780b6ead0c
n <@>
2021-11-25 00:36:16 +0200

    k.c

--- k.c
+++ k.c
@@ -5,12 +5,12 @@
 C TK(Kx)_(Tk[xt])
 N NK(Kx)_(xn)
 V*dK(Kx)_(xV)
-S V VK(V*a,Kx){Mc(a,xV,xn*xW);}
+S V VK(Ct,V*a,Kx){x=cT[t](x);Mc(a,xV,xn*TZ[t]);}
 
-K Kc(Cv)_(ac(v))     C cK(Kx)_(xv)  K KC(C*a,Nn)_(aV(tC,n,a))                   V CK(C*a,Kx){VK(a,x);}
+K Kc(Cv)_(ac(v))     C cK(Kx)_(xv)  K KC(C*a,Nn)_(aV(tC,n,a))                   V CK(C*a,Kx){VK(tC,a,x);}
 K Ks(C*s)_(syP(s))                  K KS(C**a,Nn)_(Ax=aS(n);i(n,xi=syP(a[i]))x)
-K Ki(Iv)_(ai(v))     I iK(Kx)_(xv)  K KI(I*a,Nn)_(aV(tI,n,a))                   V IK(I*a,Kx){VK(a,x);}
-K Kf(Dv)_(ad(v))     D fK(Kx)_(*xD) K KF(D*a,Nn)_(aV(tD,n,a))                   V FK(D*a,Kx){VK(a,x);}
+K Ki(Iv)_(ai(v))     I iK(Kx)_(xv)  K KI(I*a,Nn)_(aV(tI,n,a))                   V IK(I*a,Kx){VK(tI,a,x);}
+K Kf(Dv)_(ad(v))     D fK(Kx)_(*xD) K KF(D*a,Nn)_(aV(tD,n,a))                   V FK(D*a,Kx){VK(tD,a,x);}
 K Kp(V*p)_(al((L)p))
 
 K K1(Cv,Kx)_(v1[si(vc,v)](x))
